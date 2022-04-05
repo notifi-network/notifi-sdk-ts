2b494b2a5cb6d55f5968abda95c3b92913ce7260
juni-notifi <100658137+juni-notifi@users.noreply.github.com>
2022-04-05 12:39:35 -0700

    Move JWT storage to localforage (#17)
    * Move to localforage
    
    There are some clients for whom localStorage is not the right solution.
    
    Migrate to localforge to abstract underlying storage one more step
    
    * Catch errors inside hook
    
    There's probably a better error handling pattern. However, a failure to
    persist the JWT isn't the end of the world, so for now, just catch the
    issues inside the hook.

--- packages/notifi-react-hooks/lib/hooks/useNotifiJwt.ts
+++ packages/notifi-react-hooks/lib/hooks/useNotifiJwt.ts
@@ -1,5 +1,9 @@
-import { useCallback, useEffect, useRef } from 'react';
-import useLocalStorageState from 'use-local-storage-state';
+import localforage from 'localforage';
+import React, { useCallback, useMemo, useRef } from 'react';
+
+localforage.config({
+  name: 'notifi',
+});
 
 const useNotifiJwt = (
   dappAddress: string,
@@ -9,22 +13,36 @@ const useNotifiJwt = (
   jwtRef: React.MutableRefObject<string | null>;
   setJwt: (jwt: string | null) => void;
 }> => {
-  const [storage, setStorage] = useLocalStorageState<string | null>(
-    `${jwtPrefix}:${dappAddress}:${walletPublicKey}`,
-    { defaultValue: null },
+  const key = useMemo(
+    () => `${jwtPrefix}:${dappAddress}:${walletPublicKey}`,
+    [jwtPrefix, dappAddress, walletPublicKey],
   );
 
-  const jwtRef = useRef<string | null>(storage);
-  useEffect(() => {
-    jwtRef.current = storage;
-  }, [storage]);
+  const jwtRef = useRef<string | null>(null);
+
+  React.useEffect(() => {
+    const getItem = async () => {
+      const value = await localforage.getItem<string>(key);
+      jwtRef.current = value;
+    };
+
+    getItem().catch((_e: unknown) => {
+      jwtRef.current = null;
+    });
+  }, [key]);
 
   const setJwt = useCallback(
     (jwt: string | null): void => {
+      const setItem = async () => {
+        await localforage.setItem(key, jwt);
+      };
+
       jwtRef.current = jwt;
-      setStorage(jwt);
+      setItem().catch((_e: unknown) => {
+        /* Intentionally ignore failure to save Jwt */
+      });
     },
-    [jwtRef, setStorage],
+    [jwtRef, key],
   );
 
   return { jwtRef, setJwt };
