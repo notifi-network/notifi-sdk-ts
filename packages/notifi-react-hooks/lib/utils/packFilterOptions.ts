09891f670e993c3d8c0b4bb2abc2c5808ce1325f
juni-notifi <100658137+juni-notifi@users.noreply.github.com>
2022-07-21 10:52:28 -0700

    Add `values` key to filterOptions (#52)
    * Add `values` key to filterOptions
    
    This key allows us to configure with either an `and` or `or` to evaluate
    some values. Each item has a `key` `op` and `value`.
    
    * Serialize Client Options always
    
    I forget why we did this before, but this is better to just serialize
    the whole thing I think.

--- packages/notifi-react-hooks/lib/utils/packFilterOptions.ts
+++ packages/notifi-react-hooks/lib/utils/packFilterOptions.ts
@@ -3,25 +3,11 @@ import type { FilterOptions } from '@notifi-network/notifi-core';
 const packFilterOptions = (
   clientOptions: Readonly<FilterOptions> | undefined,
 ): string => {
-  const record: Record<string, string> = {};
-
-  if (clientOptions !== undefined) {
-    const { alertFrequency, directMessageType, threshold } = clientOptions;
-
-    if (alertFrequency !== undefined) {
-      record.alertFrequency = alertFrequency;
-    }
-
-    if (directMessageType !== undefined) {
-      record.directMessageType = directMessageType;
-    }
-
-    if (threshold !== undefined) {
-      record.threshold = threshold.toString();
-    }
+  if (clientOptions === undefined) {
+    return '{}';
   }
 
-  return JSON.stringify(record);
+  return JSON.stringify(clientOptions);
 };
 
 export default packFilterOptions;
