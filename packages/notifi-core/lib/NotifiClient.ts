49b0e80fc8fe34a1442ce9fc8f7da6b174302ee4
juni-notifi <hyungjoon.kim@notifi.network>
2022-07-20 22:26:12 -0700

    Add `values` key to filterOptions
    This key allows us to configure with either an `and` or `or` to evaluate
    some values. Each item has a `key` `op` and `value`.

--- packages/notifi-core/lib/NotifiClient.ts
+++ packages/notifi-core/lib/NotifiClient.ts
@@ -29,11 +29,21 @@ export type AlertFrequency =
   | 'HOURLY'
   | 'DAILY';
 
+export type ValueItemConfig = Readonly<{
+  key: string;
+  op: 'lt' | 'lte' | 'eq' | 'gt' | 'gte';
+  value: string;
+}>;
+
 export type FilterOptions = Partial<{
   alertFrequency: AlertFrequency;
   directMessageType: string;
   threshold: number;
   delayProcessingUntil: string;
+  values: Readonly<
+    | { and: ReadonlyArray<ValueItemConfig> }
+    | { or: ReadonlyArray<ValueItemConfig> }
+  >;
 }>;
 
 /**
