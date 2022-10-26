7329f08a1797c00083ac9bdab1668e2977a4d60a
juni-notifi <100658137+juni-notifi@users.noreply.github.com>
2022-10-26 16:55:05 -0700

    Small fixes (#94)
    * Add react-virtuoso dependency
    
    This is required for the new pagination.
    
    * Remove warnings about invalid dom props
    
    camelCase vs kebab-case and React

--- packages/notifi-react-card/lib/assets/backArrow.tsx
+++ packages/notifi-react-card/lib/assets/backArrow.tsx
@@ -11,12 +11,12 @@ export const BackArrow: React.FC<Props> = ({ className }: Props) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="inherit"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
       />
     </svg>
