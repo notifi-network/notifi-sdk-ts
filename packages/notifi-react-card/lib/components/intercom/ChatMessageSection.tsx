fdbb8e8c2b8d8d09c883287d76b9a64603b0e3fb
Hao Wang <ge_nah@hotmail.com>
2022-11-16 22:54:51 -1000

    render conversation messages

--- packages/notifi-react-card/lib/components/intercom/ChatMessageSection.tsx
+++ packages/notifi-react-card/lib/components/intercom/ChatMessageSection.tsx
@@ -1,40 +1,96 @@
-import clsx from 'clsx';
 import React from 'react';
+import { Virtuoso } from 'react-virtuoso';
 
-import { ChatMessageDate, ChatMessageDateProps } from './ChatMessageData';
+import { useIntercomChat } from '../../hooks/useIntercomChat';
+import { ChatMessageDate, ChatMessageDateProps } from './ChatMessageDate';
+import {
+  ChatWindowIntroSection,
+  ChatWindowIntroSectionProps,
+} from './ChatWindowIntroSection';
 import { MessageList, MessageListProps } from './MessageList';
 
 export type ChatMessageSectionProps = Readonly<{
   classNames?: Readonly<{
-    container: string;
-    content: string;
-    date: ChatMessageDateProps['classNames'];
+    chatWindowIntro: ChatWindowIntroSectionProps['classNames'];
     messageList: MessageListProps['classNames'];
+    date: ChatMessageDateProps['classNames'];
   }>;
   chatMessageSectionIntroContent?: string;
 }>;
 
 export const ChatMessageSection: React.FC<ChatMessageSectionProps> = ({
   classNames,
-  chatMessageSectionIntroContent = 'What can we help you with today?',
+  chatMessageSectionIntroContent,
 }) => {
+  const {
+    conversation,
+    setIsScrolling,
+    setVisibleRange,
+    setAtTop,
+    isLoading,
+    hasNextPage,
+  } = useIntercomChat({
+    conversationId: '8c5ccf41d93c4bb9b5e1e1014002e923',
+  });
+
   return (
-    <div
-      className={clsx(
-        'NotifiIntercomChatMessageSection__container',
-        classNames?.container,
+    <>
+      {conversation.feed.length === 0 ? (
+        <ChatWindowIntroSection
+          classNames={classNames?.chatWindowIntro}
+          startDate={conversation.createdDate}
+          chatMessageSectionIntroContent={chatMessageSectionIntroContent}
+        />
+      ) : null}
+
+      {conversation.feed.length === 0 ? null : (
+        <Virtuoso
+          atTopStateChange={setAtTop}
+          className="virtual-container"
+          data={conversation.feed}
+          followOutput="auto"
+          isScrolling={setIsScrolling}
+          rangeChanged={setVisibleRange}
+          itemContent={(index, feed) => {
+            return (
+              <div key={index}>
+                {isLoading ? (
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      padding: '10px',
+                    }}
+                  >
+                    Loading...
+                  </div>
+                ) : null}
+                {index === 0 && !hasNextPage && (
+                  <ChatWindowIntroSection
+                    classNames={classNames?.chatWindowIntro}
+                    startDate={conversation.createdDate}
+                    chatMessageSectionIntroContent={
+                      chatMessageSectionIntroContent
+                    }
+                    inVirtualContainerStyle={
+                      'ChatWindowIntro__virtualContainer'
+                    }
+                  />
+                )}
+                {index != 0 && (
+                  <ChatMessageDate
+                    classNames={classNames?.date}
+                    createdDate={feed.timestamp}
+                    isStartDate={false}
+                  />
+                )}
+                <MessageList classNames={classNames?.messageList} feed={feed} />
+              </div>
+            );
+          }}
+          style={{ width: '364px', height: '290px' }}
+        />
       )}
-    >
-      <ChatMessageDate classNames={classNames?.date} />
-      <div
-        className={clsx(
-          'NotifiIntercomChatMessageSectionIntro__content',
-          classNames?.content,
-        )}
-      >
-        {chatMessageSectionIntroContent}
-      </div>
-      <MessageList classNames={classNames?.messageList} />
-    </div>
+    </>
   );
 };
--- packages/notifi-react-card/lib/components/intercom/MessageList.tsx
+++ packages/notifi-react-card/lib/components/intercom/MessageList.tsx
@@ -1,60 +1,21 @@
 import clsx from 'clsx';
 import React from 'react';
-import { Virtuoso } from 'react-virtuoso';
 
+import { FeedEntry } from '../../hooks/useIntercomChat';
 import { MessageGroup, MessageGroupProps } from './MessageGroup';
-
-const mockConversation = [
-  {
-    direction: 'INCOMING',
-    messages: [
-      {
-        body: 'Good morning! What can we help you with today?',
-        timeStamp: '11:19 AM',
-        sender: 'Gwen',
-        avatar: 'https://i.postimg.cc/K8c0KDpZ/img-alert-transaction.png',
-      },
-    ],
-  },
-  {
-    direction: 'OUTGOING',
-    messages: [
-      {
-        body: 'I’m having an issue',
-        timeStamp: '11:20 AM',
-        sender: '',
-        avatar: '',
-      },
-      {
-        body: 'Where when I connect a wallet',
-        timeStamp: '11:21 AM',
-        sender: '',
-        avatar: '',
-      },
-      {
-        body: 'I’m having an issue',
-        timeStamp: '11:20 AM',
-        sender: '',
-        avatar: '',
-      },
-      {
-        body: 'Where when I connect a wallet',
-        timeStamp: '11:21 AM',
-        sender: '',
-        avatar: '',
-      },
-    ],
-  },
-];
 
 export type MessageListProps = Readonly<{
   classNames?: Readonly<{
     container: string;
     messageGroup: MessageGroupProps['classNames'];
   }>;
+  feed: FeedEntry;
 }>;
 
-export const MessageList: React.FC<MessageListProps> = ({ classNames }) => {
+export const MessageList: React.FC<MessageListProps> = ({
+  classNames,
+  feed,
+}) => {
   return (
     <div
       className={clsx(
@@ -62,21 +23,10 @@ export const MessageList: React.FC<MessageListProps> = ({ classNames }) => {
         classNames?.container,
       )}
     >
-      <Virtuoso
-        className="virtual-container"
-        data={mockConversation}
-        followOutput="auto"
-        itemContent={(index, feed) => {
-          return (
-            <MessageGroup
-              classNames={classNames?.messageGroup}
-              messages={feed.messages}
-              direction={feed.direction}
-              key={index}
-            />
-          );
-        }}
-        style={{ height: '290px', width: '364px' }}
+      <MessageGroup
+        classNames={classNames?.messageGroup}
+        messages={feed.messages}
+        direction={feed.direction}
       />
     </div>
   );
