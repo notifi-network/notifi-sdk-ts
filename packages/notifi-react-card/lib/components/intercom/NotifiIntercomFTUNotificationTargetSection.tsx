35d06255a280bb38e3874f36c59c7ec077a40ba5
Hao Wang <ge_nah@hotmail.com>
2022-12-01 11:42:18 -0800

    remove comments lines

--- packages/notifi-react-card/lib/components/intercom/NotifiIntercomFTUNotificationTargetSection.tsx
+++ packages/notifi-react-card/lib/components/intercom/NotifiIntercomFTUNotificationTargetSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import { IntercomCardConfigItemV1 } from '../../hooks/IntercomCardConfig';
 import { NotifiEmailInput, NotifiEmailInputProps } from '../NotifiEmailInput';
-import { NotifiSmsInput, NotifiSmsInputProps } from '../NotifiSmsInput';
+import { NotifiSmsInputProps } from '../NotifiSmsInput';
 import {
   NotifiTelegramInput,
   NotifiTelegramInputProps,
@@ -32,8 +32,6 @@ export type NotifiIntercomFTUNotificationTargetSectionProps = Readonly<{
 export const NotifiIntercomFTUNotificationTargetSection: React.FC<
   NotifiIntercomFTUNotificationTargetSectionProps
 > = ({ data, inputSeparators, classNames, inputLabels }) => {
-  const allowedCountryCodes = [...data.contactInfo.sms.supportedCountryCodes];
-
   return (
     <div className={'NotifiSupportNotificationOption__container'}>
       {data.contactInfo.email.active ? (
@@ -66,41 +64,6 @@ export const NotifiIntercomFTUNotificationTargetSection: React.FC<
           </div>
         </div>
       ) : null}
-      {/* {data.contactInfo.sms.active ? (
-        <NotifiSmsInput
-          disabled={false}
-          classNames={classNames?.NotifiSmsInput}
-          allowedCountryCodes={allowedCountryCodes}
-          copy={{ label: inputLabels?.sms }}
-          intercomSmsInputContainerStyle={'NotifiIntercomSmsInput__container'}
-          intercomSmsInputStyle={'NotifiIntercomSmsInput__input'}
-          intercomSmsDropdownContainerStyle={
-            'NotifiIntercomSmsInput__dropdownContainer'
-          }
-          intercomSmsDropdownSelectStyle={
-            'NotifiIntercomSmsInput__dropdownSelect'
-          }
-          intercomView={true}
-        />
-      ) : null}
-      {inputSeparators?.smsSeparator?.content ? (
-        <div
-          className={clsx(
-            'NotifiInputSeparator__container',
-            'NotifiIntercomInputSeparator__container',
-            inputSeparators?.smsSeparator?.classNames?.container,
-          )}
-        >
-          <div
-            className={clsx(
-              'NotifiInputSeparator__content',
-              inputSeparators.smsSeparator.classNames?.content,
-            )}
-          >
-            {inputSeparators?.smsSeparator?.content}
-          </div>
-        </div>
-      ) : null} */}
       {data.contactInfo.telegram.active ? (
         <NotifiTelegramInput
           disabled={false}
