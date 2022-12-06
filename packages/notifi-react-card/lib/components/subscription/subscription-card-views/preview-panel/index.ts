0aa5000ec608132cb0e7f8145028b329d5195424
Nathan Chung <105258726+Nathan-Notifi@users.noreply.github.com>
2022-12-06 09:46:59 -0800

    removes notifi react example from updates

---
+++ packages/notifi-react-card/lib/components/subscription/subscription-card-views/preview-panel/index.ts
@@ -0,0 +1,1 @@
+export * from './UserInfoPanel';
--- packages/notifi-react-card/lib/context/index.ts
+++ packages/notifi-react-card/lib/context/index.ts
@@ -1,3 +1,4 @@
 export * from './NotifiSubscriptionContext';
 export * from './NotifiClientContext';
 export * from './NotifiContext';
+export * from '../components/subscription/NotifiSubscriptionContextWrapper';
