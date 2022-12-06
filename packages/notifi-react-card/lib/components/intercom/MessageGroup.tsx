fe608a95d05bdeab2c919ad7544a675f7f086a74
Hao Wang <ge_nah@hotmail.com>
2022-12-05 17:56:48 -0800

    update style for avatar

--- packages/notifi-react-card/lib/components/intercom/MessageGroup.tsx
+++ packages/notifi-react-card/lib/components/intercom/MessageGroup.tsx
@@ -44,24 +44,21 @@ export const MessageGroup: React.FC<MessageGroupProps> = ({
             key={index}
           >
             {isIncoming ? (
-              <div>
-                <img
-                  height={34}
-                  src={
-                    participantProfile.avatarDataType === 'URL'
-                      ? participantProfile.avatarData
-                      : ''
-                  }
-                  onError={(e) => {
-                    const img = e.target as HTMLImageElement;
-                    img.style.display = 'none';
-                  }}
-                  className={clsx(
-                    'NotifiIntercomChatMessage__avatar',
-                    classNames?.avatar,
-                  )}
-                />
-              </div>
+              <img
+                src={
+                  participantProfile.avatarDataType === 'URL'
+                    ? participantProfile.avatarData
+                    : ''
+                }
+                onError={(e) => {
+                  const img = e.target as HTMLImageElement;
+                  img.style.display = 'none';
+                }}
+                className={clsx(
+                  'NotifiIntercomChatMessage__avatar',
+                  classNames?.avatar,
+                )}
+              />
             ) : null}
             <div
               key={index}
