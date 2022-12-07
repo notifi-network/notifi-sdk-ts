fb274116fb12e424c67a54ada7bde2e1d2ea87e3
Nathan Chung <105258726+Nathan-Notifi@users.noreply.github.com>
2022-12-07 14:31:13 -0800

    exports input fields

--- packages/notifi-react-card/lib/components/subscription/subscription-card-views/index.ts
+++ packages/notifi-react-card/lib/components/subscription/subscription-card-views/index.ts
@@ -3,5 +3,5 @@ export * from './AlertNotificationRow';
 export * from './EditCardView';
 export * from './HistoryCardView';
 export * from './PreviewCard';
-
+export * from './InputFields';
 export * from './preview-panel';
