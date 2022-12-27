3ba308322f6c2f48d9d9e09f86f5e0d02373d611
Hao Wang <ge_nah@hotmail.com>
2022-12-27 15:52:45 -0800

    empty error message when toggle alert off

--- packages/notifi-react-card/lib/components/subscription/EventTypeHealthCheckRow.tsx
+++ packages/notifi-react-card/lib/components/subscription/EventTypeHealthCheckRow.tsx
@@ -91,6 +91,7 @@ export const EventTypeHealthCheckRow: React.FC<
   ]);
 
   const handleToggleNewSubscription = useCallback(() => {
+    setErrorMessage('');
     if (loading) {
       return;
     }
