b7d9504f8ced96467a71f0d3ff720bd972707b15
Nathan-Notifi <105258726+Nathan-Notifi@users.noreply.github.com>
2023-02-28 14:39:18 -0800

    updates svg casing (#197)

--- packages/notifi-react-card/lib/assets/ClockIcon.tsx
+++ packages/notifi-react-card/lib/assets/ClockIcon.tsx
@@ -15,8 +15,8 @@ export const ClockIcon: React.FC<Props> = (props: Props) => {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12ZM5.25 2V6.5V6.81066L5.46967 7.03033L7.96967 9.53033L9.03033 8.46967L6.75 6.18934V2H5.25Z"
         fill="currentColor"
       />
--- packages/notifi-react-card/lib/assets/EventCheckmarkIcon.tsx
+++ packages/notifi-react-card/lib/assets/EventCheckmarkIcon.tsx
@@ -12,7 +12,7 @@ export const EventCheckmarkIcon: React.FC<Props> = (props: Props) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M1 5L4 8.5L11.5 1" stroke="currentColor" stroke-width="2" />
+      <path d="M1 5L4 8.5L11.5 1" stroke="currentColor" strokeWidth="2" />
     </svg>
   );
 };
