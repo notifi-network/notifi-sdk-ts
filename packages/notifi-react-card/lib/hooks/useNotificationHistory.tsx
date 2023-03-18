529c47e5a00b1453dcbda6da1a77667b466c65fc
eric-notifi <macbookpro4eric@Erics-MacBook-Pro.local>
2023-03-18 09:15:59 +0900

    Memoize the supportedEventDetails map

--- packages/notifi-react-card/lib/hooks/useNotificationHistory.tsx
+++ packages/notifi-react-card/lib/hooks/useNotificationHistory.tsx
@@ -6,7 +6,7 @@ import {
   HealthValueOverThresholdEventDetails,
   NotificationHistoryEntry,
 } from '@notifi-network/notifi-core';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { AnnouncementIcon } from '../assets/AnnouncementIcon';
 import { ChatAlertIcon } from '../assets/ChatAlertIcon';
@@ -34,130 +34,134 @@ export const useNotificationHistory = () => {
     }
   >;
 
-  const supportedEventDetails: SupportedEventDetailPropsMap = new Map();
-  supportedEventDetails.set('BroadcastMessageEventDetails', {
-    getViewProps: (notification: NotificationHistoryEntry) => {
-      const detail = notification.detail as BroadcastMessageEventDetails;
-      return {
-        notificationTitle: 'Announcement',
-        notificationImage: <AnnouncementIcon />,
-        notificationSubject: detail.subject ?? '',
-        notificationDate: notification.createdDate,
-        notificationMessage: detail.message ?? '',
-      };
-    },
-    getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
-      const detail = notification.detail as BroadcastMessageEventDetails;
-      return {
-        topContent: detail.subject ?? '',
-        bottomContent: detail.message ?? '',
-      };
-    },
-  });
-  supportedEventDetails.set('HealthValueOverThresholdEventDetails', {
-    getViewProps: (notification: NotificationHistoryEntry) => {
-      const detail =
-        notification.detail as HealthValueOverThresholdEventDetails;
-      const threshold = detail.threshold ?? '';
-      const name = detail.name ?? '';
-      const value = detail.value ?? '';
-      let thresholdDirection = '';
-      if (parseFloat(value) > parseFloat(threshold)) {
-        thresholdDirection = 'under';
-      } else {
-        thresholdDirection = 'over';
-      }
-      return {
-        notificationTitle: 'Health Check',
-        notificationImage: <RatioCheckIcon />,
-        notificationSubject: `${name} ${thresholdDirection} ${threshold}`,
-        notificationDate: notification.createdDate,
-        notificationMessage: undefined,
-      };
-    },
-    getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
-      const detail =
-        notification.detail as HealthValueOverThresholdEventDetails;
-      return {
-        topContent: detail.name,
-        bottomContent: `value: ${detail.value}`,
-        otherContent: `threshold: ${detail.threshold}`,
-      };
-    },
-  });
-  supportedEventDetails.set('GenericEventDetails', {
-    getViewProps: (notification) => {
-      const detail = notification.detail as GenericEventDetails;
-      return {
-        notificationTitle: detail.sourceName,
-        notificationImage: <AlertIcon icon={detail.icon} />,
-        notificationSubject: detail.notificationTypeName,
-        notificationDate: notification.createdDate,
-        notificationMessage: detail.genericMessage,
-      };
-    },
-    getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
-      const detail = notification.detail as GenericEventDetails;
-      return {
-        topContent: detail.notificationTypeName,
-        bottomContent: detail.genericMessage,
-      };
-    },
-  });
-  supportedEventDetails.set('ChatMessageReceivedEventDetails', {
-    getViewProps: (notification) => {
-      const detail = notification.detail as ChatMessageReceivedEventDetails;
-      return {
-        notificationTitle: `New Message from ${detail.senderName}`,
-        notificationSubject: `New Message from ${detail.senderName}`,
-        notificationDate: notification.createdDate,
-        notificationMessage: detail.messageBody,
-        notificationImage: <ChatAlertIcon width={17} height={17} />,
-      };
-    },
-    getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
-      const detail = notification.detail as ChatMessageReceivedEventDetails;
-      return {
-        topContent: `New Message from ${detail.senderName}`,
-        bottomContent: detail.messageBody,
-      };
-    },
-  });
-  supportedEventDetails.set('AccountBalanceChangedEventDetails', {
-    getViewProps: (notification) => {
-      const detail = notification.detail as AccountBalanceChangedEventDetails;
-      const changeAmount = formatAmount(
-        Math.abs(detail.previousValue - detail.newValue),
-      );
-      const getTitle = () => {
-        return detail.direction === 'INCOMING'
-          ? `Incoming Transaction: ${changeAmount} ${detail.tokenSymbol}`
-          : `Outgoing Transaction: ${changeAmount} ${detail.tokenSymbol}`;
-      };
-      const walletAddress = notification.sourceAddress ?? '';
-      return {
-        notificationImage: <SwapIcon />,
-        notificationTitle: getTitle(),
-        notificationSubject: getTitle(),
-        notificationDate: notification.createdDate,
-        notificationMessage: `Wallet ${walletAddress} account balance changed by ${changeAmount} ${detail.tokenSymbol}`,
-      };
-    },
-    getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
-      const detail = notification.detail as AccountBalanceChangedEventDetails;
-      const changeAmount = `${formatAmount(
-        Math.abs(detail.previousValue - detail.newValue),
-      )}`;
-      const topContent =
-        detail.direction === 'INCOMING'
-          ? `Incoming Transaction: ${changeAmount}  ${detail.tokenSymbol}`
-          : `Outgoing Transaction: ${changeAmount}  ${detail.tokenSymbol}`;
-      return {
-        topContent,
-        bottomContent: `Wallet ${notification.sourceAddress} account balance changed by ${changeAmount} ${detail.tokenSymbol}`,
-      };
-    },
-  });
+  const supportedEventDetails: SupportedEventDetailPropsMap = useMemo(() => {
+    const details = new Map();
+
+    details.set('BroadcastMessageEventDetails', {
+      getViewProps: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as BroadcastMessageEventDetails;
+        return {
+          notificationTitle: 'Announcement',
+          notificationImage: <AnnouncementIcon />,
+          notificationSubject: detail.subject ?? '',
+          notificationDate: notification.createdDate,
+          notificationMessage: detail.message ?? '',
+        };
+      },
+      getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as BroadcastMessageEventDetails;
+        return {
+          topContent: detail.subject ?? '',
+          bottomContent: detail.message ?? '',
+        };
+      },
+    });
+    details.set('HealthValueOverThresholdEventDetails', {
+      getViewProps: (notification: NotificationHistoryEntry) => {
+        const detail =
+          notification.detail as HealthValueOverThresholdEventDetails;
+        const threshold = detail.threshold ?? '';
+        const name = detail.name ?? '';
+        const value = detail.value ?? '';
+        let thresholdDirection = '';
+        if (parseFloat(value) > parseFloat(threshold)) {
+          thresholdDirection = 'under';
+        } else {
+          thresholdDirection = 'over';
+        }
+        return {
+          notificationTitle: 'Health Check',
+          notificationImage: <RatioCheckIcon />,
+          notificationSubject: `${name} ${thresholdDirection} ${threshold}`,
+          notificationDate: notification.createdDate,
+          notificationMessage: undefined,
+        };
+      },
+      getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
+        const detail =
+          notification.detail as HealthValueOverThresholdEventDetails;
+        return {
+          topContent: detail.name,
+          bottomContent: `value: ${detail.value}`,
+          otherContent: `threshold: ${detail.threshold}`,
+        };
+      },
+    });
+    details.set('GenericEventDetails', {
+      getViewProps: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as GenericEventDetails;
+        return {
+          notificationTitle: detail.sourceName,
+          notificationImage: <AlertIcon icon={detail.icon} />,
+          notificationSubject: detail.notificationTypeName,
+          notificationDate: notification.createdDate,
+          notificationMessage: detail.genericMessage,
+        };
+      },
+      getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as GenericEventDetails;
+        return {
+          topContent: detail.notificationTypeName,
+          bottomContent: detail.genericMessage,
+        };
+      },
+    });
+    details.set('ChatMessageReceivedEventDetails', {
+      getViewProps: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as ChatMessageReceivedEventDetails;
+        return {
+          notificationTitle: `New Message from ${detail.senderName}`,
+          notificationSubject: `New Message from ${detail.senderName}`,
+          notificationDate: notification.createdDate,
+          notificationMessage: detail.messageBody,
+          notificationImage: <ChatAlertIcon width={17} height={17} />,
+        };
+      },
+      getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as ChatMessageReceivedEventDetails;
+        return {
+          topContent: `New Message from ${detail.senderName}`,
+          bottomContent: detail.messageBody,
+        };
+      },
+    });
+    details.set('AccountBalanceChangedEventDetails', {
+      getViewProps: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as AccountBalanceChangedEventDetails;
+        const changeAmount = formatAmount(
+          Math.abs(detail.previousValue - detail.newValue),
+        );
+        const getTitle = () => {
+          return detail.direction === 'INCOMING'
+            ? `Incoming Transaction: ${changeAmount} ${detail.tokenSymbol}`
+            : `Outgoing Transaction: ${changeAmount} ${detail.tokenSymbol}`;
+        };
+        const walletAddress = notification.sourceAddress ?? '';
+        return {
+          notificationImage: <SwapIcon />,
+          notificationTitle: getTitle(),
+          notificationSubject: getTitle(),
+          notificationDate: notification.createdDate,
+          notificationMessage: `Wallet ${walletAddress} account balance changed by ${changeAmount} ${detail.tokenSymbol}`,
+        };
+      },
+      getAlertDetailsContents: (notification: NotificationHistoryEntry) => {
+        const detail = notification.detail as AccountBalanceChangedEventDetails;
+        const changeAmount = `${formatAmount(
+          Math.abs(detail.previousValue - detail.newValue),
+        )}`;
+        const topContent =
+          detail.direction === 'INCOMING'
+            ? `Incoming Transaction: ${changeAmount}  ${detail.tokenSymbol}`
+            : `Outgoing Transaction: ${changeAmount}  ${detail.tokenSymbol}`;
+        return {
+          topContent,
+          bottomContent: `Wallet ${notification.sourceAddress} account balance changed by ${changeAmount} ${detail.tokenSymbol}`,
+        };
+      },
+    });
+    return details;
+  }, []);
 
   const validateIsSupported = (entry?: NotificationHistoryEntry): boolean => {
     if (supportedEventDetails.get(entry?.detail?.__typename ?? '')) return true;
