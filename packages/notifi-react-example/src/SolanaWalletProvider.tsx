a4d8d68142a6b1a24598c372fc086f99c839e18a
juni-notifi <hyungjoon.kim@notifi.network>
2023-03-18 11:24:12 -0700

    Upgrade React in build
    Use version 18 as peer dependency in our dev builds.
    
    In the process, I found that the latest version of graphql-codegen was
    improperly using a transitive dependency. Resolved it by adding an
    explicit dependency on an older version of the graphql-request library

--- packages/notifi-react-example/src/SolanaWalletProvider.tsx
+++ packages/notifi-react-example/src/SolanaWalletProvider.tsx
@@ -1,24 +1,26 @@
-import React, { FC, useMemo } from 'react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
   ConnectionProvider,
   WalletProvider,
 } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  WalletDisconnectButton,
+  WalletModalProvider,
+  WalletMultiButton,
+} from '@solana/wallet-adapter-react-ui';
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
+import React, { FC, PropsWithChildren, useMemo } from 'react';
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
 
-export const SolanaWalletProvider: FC = ({ children }) => {
+export const SolanaWalletProvider: FC<PropsWithChildren<{}>> = ({
+  children,
+}: React.PropsWithChildren<{}>) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
   const network = WalletAdapterNetwork.Devnet;
 
--- packages/notifi-react-example/src/index.tsx
+++ packages/notifi-react-example/src/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 
 import { AcalaWalletContextProvider } from './AcalaWalletContextProvider';
 import App from './App';
 import { SolanaWalletProvider } from './SolanaWalletProvider';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AcalaWalletContextProvider>
-      <SolanaWalletProvider>
-        <App />
-      </SolanaWalletProvider>
-    </AcalaWalletContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+if (container != null) {
+  const root = ReactDOMClient.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <AcalaWalletContextProvider>
+        <SolanaWalletProvider>
+          <App />
+        </SolanaWalletProvider>
+      </AcalaWalletContextProvider>
+    </React.StrictMode>,
+  );
+}
