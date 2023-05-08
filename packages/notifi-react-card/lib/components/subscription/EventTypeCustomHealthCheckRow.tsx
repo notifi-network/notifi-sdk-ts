9bde554d3804d636be8114a28020d67ce232eb32
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2023-05-08 15:24:59 +0900

    Implement alternative 'subscribe' method using frontendClient in 'EventTypeCustomHealthCheckRow' component (#296)
    Co-authored-by: eric-notifi <macbookpro4eric@Erics-MacBook-Pro.local>

--- packages/notifi-react-card/lib/components/subscription/EventTypeCustomHealthCheckRow.tsx
+++ packages/notifi-react-card/lib/components/subscription/EventTypeCustomHealthCheckRow.tsx
@@ -1,6 +1,13 @@
 import { ThresholdDirection } from '@notifi-network/notifi-core';
-import { CustomTopicTypeItem } from '@notifi-network/notifi-frontend-client';
+import {
+  CustomTopicTypeItem,
+  EventTypeItem,
+} from '@notifi-network/notifi-frontend-client';
 import clsx from 'clsx';
+import {
+  subscribeAlertByFrontendClient,
+  unsubscribeAlertByFrontendClient,
+} from 'notifi-react-card/lib/utils/frontendClient';
 import React, {
   useCallback,
   useEffect,
@@ -9,8 +16,11 @@ import React, {
   useState,
 } from 'react';
 
-import { useNotifiSubscriptionContext } from '../../context';
-import { useNotifiSubscribe } from '../../hooks';
+import {
+  useNotifiClientContext,
+  useNotifiSubscriptionContext,
+} from '../../context';
+import { SubscriptionData, useNotifiSubscribe } from '../../hooks';
 import { DeepPartialReadonly, customThresholdConfiguration } from '../../utils';
 import type { NotifiToggleProps } from './NotifiToggle';
 import { NotifiToggle } from './NotifiToggle';
@@ -48,10 +58,15 @@ export const EventTypeCustomHealthCheckRow: React.FC<
   disabled,
   inputs,
 }: EventTypeCustomHealthCheckRowProps) => {
-  const { alerts, loading } = useNotifiSubscriptionContext();
+  const { alerts, loading, render } = useNotifiSubscriptionContext();
   const { instantSubscribe } = useNotifiSubscribe({
     targetGroupName: 'Default',
   });
+
+  const {
+    canary: { isActive: isCanaryActive, frontendClient },
+  } = useNotifiClientContext();
+
   const [enabled, setEnabled] = useState(false);
   // This indicates which box to select
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
@@ -119,6 +134,62 @@ export const EventTypeCustomHealthCheckRow: React.FC<
     }
   }, [alertName, alerts, loading, enabled, setEnabled, isNotificationLoading]);
 
+  const subscribeAlert = useCallback(
+    async (
+      alertDetail: Readonly<{
+        eventType: EventTypeItem;
+        inputs: Record<string, unknown>;
+      }>,
+      ratioNumber: number,
+    ): Promise<SubscriptionData> => {
+      if (isCanaryActive) {
+        alertDetail.inputs[`${alertDetail.eventType.name}__healthRatio`] =
+          ratioNumber;
+        return subscribeAlertByFrontendClient(frontendClient, alertDetail);
+      } else {
+        return instantSubscribe({
+          alertConfiguration: customThresholdConfiguration({
+            sourceType: config.sourceType,
+            filterType: config.filterType,
+            alertFrequency: config.alertFrequency,
+            sourceAddress: resolveStringRef(
+              alertName,
+              config.sourceAddress,
+              inputs,
+            ),
+            thresholdDirection:
+              config.checkRatios[0].type ?? thresholdDirection,
+            threshold:
+              config.numberType === 'percentage'
+                ? ratioNumber / 100
+                : ratioNumber,
+          }),
+          alertName,
+        });
+      }
+    },
+    [isCanaryActive, frontendClient],
+  );
+
+  const unSubscribeAlert = useCallback(
+    async (
+      alertDetail: Readonly<{
+        eventType: EventTypeItem;
+        inputs: Record<string, unknown>;
+      }>,
+    ) => {
+      if (isCanaryActive) {
+        return unsubscribeAlertByFrontendClient(frontendClient, alertDetail);
+      } else {
+        return instantSubscribe({
+          alertName: alertDetail.eventType.name,
+          alertConfiguration: null,
+        });
+      }
+    },
+    [isCanaryActive, frontendClient],
+  );
+
   const handleCustomRatioButtonNewSubscription = () => {
     if (loading || isNotificationLoading) {
       return;
@@ -135,28 +206,7 @@ export const EventTypeCustomHealthCheckRow: React.FC<
           : parseFloat(customInputRef.current.value);
 
       if (ratioNumber && customValue) {
-        const checkRatios = config.checkRatios;
-
-        const thresholdDirection = checkRatios[0].type ?? 'below';
-
-        instantSubscribe({
-          alertConfiguration: customThresholdConfiguration({
-            sourceType: config.sourceType,
-            filterType: config.filterType,
-            alertFrequency: config.alertFrequency,
-            sourceAddress: resolveStringRef(
-              alertName,
-              config.sourceAddress,
-              inputs,
-            ),
-            thresholdDirection: thresholdDirection,
-            threshold:
-              config.numberType === 'percentage'
-                ? ratioNumber / 100
-                : ratioNumber,
-          }),
-          alertName,
-        })
+        subscribeAlert({ eventType: config, inputs }, ratioNumber)
           .then(() => setSelectedIndex(3))
           .catch(() => setErrorMessage(UNABLE_TO_UNSUBSCRIBE))
           .finally(() => {
@@ -186,22 +236,9 @@ export const EventTypeCustomHealthCheckRow: React.FC<
 
     setErrorMessage('');
     if (value) {
-      instantSubscribe({
-        alertConfiguration: customThresholdConfiguration({
-          sourceType: config.sourceType,
-          filterType: config.filterType,
-          alertFrequency: config.alertFrequency,
-          sourceAddress: resolveStringRef(
-            alertName,
-            config.sourceAddress,
-            inputs,
-          ),
-          thresholdDirection: thresholdDirection,
-          threshold: value,
-        }),
-        alertName,
-      })
+      subscribeAlert({ eventType: config, inputs }, value)
         .then(() => {
+          isCanaryActive && frontendClient.fetchData().then(render);
           setSelectedIndex(index);
           setCustomValue('');
         })
@@ -222,29 +259,14 @@ export const EventTypeCustomHealthCheckRow: React.FC<
     setIsNotificationLoading(true);
     setErrorMessage('');
     if (!enabled && initialRatio !== null) {
-      setEnabled(true);
-
-      instantSubscribe({
-        alertConfiguration: customThresholdConfiguration({
-          sourceType: config.sourceType,
-          filterType: config.filterType,
-          alertFrequency: config.alertFrequency,
-          sourceAddress: resolveStringRef(
-            alertName,
-            config.sourceAddress,
-            inputs,
-          ),
-          thresholdDirection: thresholdDirection,
-          threshold: initialRatio,
-        }),
-        alertName,
-      })
+      subscribeAlert({ eventType: config, inputs }, initialRatio)
         .then((res) => {
           // We update optimistically so we need to check if the alert exists.
           const responseHasAlert = res.alerts[alertName] !== undefined;
           if (responseHasAlert !== true) {
             setEnabled(false);
           }
+          isCanaryActive && frontendClient.fetchData().then(render);
         })
         .catch(() => {
           setErrorMessage(UNABLE_TO_SUBSCRIBE);
@@ -254,18 +276,17 @@ export const EventTypeCustomHealthCheckRow: React.FC<
           setIsNotificationLoading(false);
         });
     } else {
-      setEnabled(false);
-      instantSubscribe({
-        alertConfiguration: null,
-        alertName: alertName,
-      })
+      unSubscribeAlert({ eventType: config, inputs })
         .then((res) => {
           setCustomValue('');
-          const responseHasAlert = res.alerts[alertName] !== undefined;
-
-          if (responseHasAlert !== false) {
-            setEnabled(true);
+          if (res) {
+            const responseHasAlert = res.alerts[alertName] !== undefined;
+            if (responseHasAlert !== false) {
+              setEnabled(true);
+            }
           }
+          // Else, ensured by frontendClient
+          isCanaryActive && frontendClient.fetchData().then(render);
         })
         .catch(() => {
           setErrorMessage(UNABLE_TO_SUBSCRIBE);
