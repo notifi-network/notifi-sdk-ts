0ec7e6ab7ecdd1d5a228d9bd88620ca7911ca7c6
eric-notifi <macbookpro4eric@Erics-MacBook-Pro.local>
2023-05-09 15:42:25 +0800

    Implement alternative 'subscribe' method using frontendClient in 'EventTypePriceChangeRow' component

--- packages/notifi-react-card/lib/components/subscription/EventTypePriceChangeRow.tsx
+++ packages/notifi-react-card/lib/components/subscription/EventTypePriceChangeRow.tsx
@@ -1,4 +1,9 @@
+import { EventTypeItem } from '@notifi-network/notifi-frontend-client';
 import clsx from 'clsx';
+import {
+  subscribeAlertByFrontendClient,
+  unsubscribeAlertByFrontendClient,
+} from 'notifi-react-card/lib/utils/frontendClient';
 import React, {
   useCallback,
   useEffect,
@@ -7,8 +12,15 @@ import React, {
   useState,
 } from 'react';
 
-import { useNotifiSubscriptionContext } from '../../context';
-import { PriceChangeEventTypeItem, useNotifiSubscribe } from '../../hooks';
+import {
+  useNotifiClientContext,
+  useNotifiSubscriptionContext,
+} from '../../context';
+import {
+  PriceChangeEventTypeItem,
+  SubscriptionData,
+  useNotifiSubscribe,
+} from '../../hooks';
 import {
   AlertConfiguration,
   DeepPartialReadonly,
@@ -33,10 +45,15 @@ export type EventTypePriceChangeRowProps = Readonly<{
 export const EventTypePriceChangeRow: React.FC<
   EventTypePriceChangeRowProps
 > = ({ classNames, disabled, config }: EventTypePriceChangeRowProps) => {
-  const { alerts, loading } = useNotifiSubscriptionContext();
+  const { alerts, loading, render } = useNotifiSubscriptionContext();
   const { instantSubscribe } = useNotifiSubscribe({
     targetGroupName: 'Default',
   });
+
+  const {
+    canary: { isActive: isCanaryActive, frontendClient },
+  } = useNotifiClientContext();
+
   const [enabled, setEnabled] = useState(false);
   const [isNotificationLoading, setIsNotificationLoading] =
     useState<boolean>(false);
@@ -61,6 +78,43 @@ export const EventTypePriceChangeRow: React.FC<
     didFetch.current = true;
   }, [alertName, alerts]);
 
+  const subscribeAlert = useCallback(
+    async (
+      alertDetail: Readonly<{
+        eventType: EventTypeItem;
+        inputs: Record<string, unknown>;
+      }>,
+    ): Promise<SubscriptionData> => {
+      if (isCanaryActive) {
+        return subscribeAlertByFrontendClient(frontendClient, alertDetail);
+      } else {
+        return instantSubscribe({
+          alertConfiguration: alertConfiguration,
+          alertName: alertName,
+        });
+      }
+    },
+    [isCanaryActive, frontendClient, config],
+  );
+  const unSubscribeAlert = useCallback(
+    async (
+      alertDetail: Readonly<{
+        eventType: EventTypeItem;
+        inputs: Record<string, unknown>;
+      }>,
+    ) => {
+      if (isCanaryActive) {
+        return unsubscribeAlertByFrontendClient(frontendClient, alertDetail);
+      } else {
+        return instantSubscribe({
+          alertName: alertDetail.eventType.name,
+          alertConfiguration: null,
+        });
+      }
+    },
+    [isCanaryActive, frontendClient],
+  );
+
   const handleNewSubscription = useCallback(() => {
     if (loading || isNotificationLoading) {
       return;
@@ -69,9 +123,9 @@ export const EventTypePriceChangeRow: React.FC<
 
     if (!enabled) {
       setEnabled(true);
-      instantSubscribe({
-        alertConfiguration: alertConfiguration,
-        alertName: alertName,
+      subscribeAlert({
+        eventType: config,
+        inputs: {},
       })
         .then((res) => {
           // We update optimistically so we need to check if the alert exists.
@@ -79,6 +133,7 @@ export const EventTypePriceChangeRow: React.FC<
           if (responseHasAlert !== true) {
             setEnabled(false);
           }
+          isCanaryActive && frontendClient.fetchData().then(render);
         })
         .catch(() => {
           setEnabled(false);
@@ -88,16 +143,20 @@ export const EventTypePriceChangeRow: React.FC<
         });
     } else {
       setEnabled(false);
-      instantSubscribe({
-        alertConfiguration: null,
-        alertName: alertName,
+      unSubscribeAlert({
+        eventType: config,
+        inputs: {},
       })
         .then((res) => {
           // We update optimistically so we need to check if the alert exists.
-          const responseHasAlert = res.alerts[alertName] !== undefined;
-          if (responseHasAlert !== false) {
-            setEnabled(true);
+          if (res) {
+            const responseHasAlert = res.alerts[alertName] !== undefined;
+            if (responseHasAlert !== false) {
+              setEnabled(true);
+            }
           }
+          // Else, ensured by frontendClient
+          isCanaryActive && frontendClient.fetchData().then(render);
         })
         .catch(() => {
           setEnabled(false);
