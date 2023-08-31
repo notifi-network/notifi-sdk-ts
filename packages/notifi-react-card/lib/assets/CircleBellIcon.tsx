0a65cf032cf3b765a59b6440aeca898a44f62cdf
Hao Wang <ge_nah@hotmail.com>
2023-08-31 10:08:14 -0700

    fix line break issue

--- packages/notifi-react-card/lib/assets/CircleBellIcon.tsx
+++ packages/notifi-react-card/lib/assets/CircleBellIcon.tsx
@@ -18,7 +18,7 @@ export const CircleBellIcon: React.FC<Props> = (props: Props) => {
         r="9.5"
         fill="white"
         stroke="url(#paint0_linear_1316_3580)"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <path
         d="M10.4901 16.1875C11.1212 16.1875 11.6376 15.6712 11.6376 15.0401H9.3427C9.3427 15.6712 9.85331 16.1875 10.4901 16.1875ZM13.9324 12.7452V9.8766C13.9324 8.11529 12.9915 6.64083 11.3507 6.25071V5.86058C11.3507 5.38439 10.9663 5 10.4901 5C10.0139 5 9.62956 5.38439 9.62956 5.86058V6.25071C7.98299 6.64083 7.04783 8.10955 7.04783 9.8766V12.7452L5.90039 13.8926V14.4663H15.0799V13.8926L13.9324 12.7452Z"
@@ -33,8 +33,8 @@ export const CircleBellIcon: React.FC<Props> = (props: Props) => {
           y2="19"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#FE7970" />
-          <stop offset="1" stop-color="#FEB776" />
+          <stop stopColor="#FE7970" />
+          <stop offset="1" stopColor="#FEB776" />
         </linearGradient>
       </defs>
     </svg>
