2d3748f1de27539d5610c94d2a238dd8885a6dd5
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2023-10-12 04:25:17 +0800

    MVP-3445: Fix Solana 'use hardwallet' not showing when destination is not required (#395)
    Co-authored-by: eric-notifi <macbookpro4eric@Mac-mini.local>

--- packages/notifi-react-card/lib/components/subscription/subscription-card-views/InputFields.tsx
+++ packages/notifi-react-card/lib/components/subscription/subscription-card-views/InputFields.tsx
@@ -28,6 +28,7 @@ export type InputFieldsProps = {
   inputTextFields?: NotifiInputFieldsText;
   allowedCountryCodes: string[];
   inputDisabled: boolean;
+  hideContactInputs?: boolean;
   classNames?: Readonly<{
     NotifiEmailInput?: NotifiEmailInputProps['classNames'];
     NotifiSmsInput?: NotifiSmsInputProps['classNames'];
@@ -43,101 +44,105 @@ export const InputFields: React.FC<InputFieldsProps> = ({
   inputTextFields,
   allowedCountryCodes,
   inputDisabled,
+  hideContactInputs,
 }) => {
   const { params } = useNotifiClientContext();
 
   return (
     <>
-      {data.contactInfo.email.active ? (
-        <NotifiEmailInput
-          disabled={inputDisabled}
-          classNames={classNames?.NotifiEmailInput}
-          copy={{
-            label: inputTextFields?.label?.email,
-            placeholder: inputTextFields?.placeholderText?.email,
-          }}
-        />
-      ) : null}
-      {inputSeparators?.emailSeparator?.content ? (
-        <div
-          className={clsx(
-            'NotifiInputSeparator__container',
-            inputSeparators?.emailSeparator?.classNames?.container,
-          )}
-        >
-          <div
-            className={clsx(
-              'NotifiInputSeparator__content',
-              inputSeparators.emailSeparator.classNames?.content,
-            )}
-          >
-            {inputSeparators?.emailSeparator?.content}
-          </div>
-        </div>
-      ) : null}
-      {data.contactInfo.sms.active ? (
-        <NotifiSmsInput
-          disabled={inputDisabled}
-          classNames={classNames?.NotifiSmsInput}
-          allowedCountryCodes={allowedCountryCodes}
-          copy={{
-            label: inputTextFields?.label?.sms,
-            placeholder: inputTextFields?.placeholderText?.sms,
-          }}
-        />
-      ) : null}
-      {inputSeparators?.smsSeparator?.content ? (
-        <div
-          className={clsx(
-            'NotifiInputSeparator__container',
-            inputSeparators?.smsSeparator?.classNames?.container,
-          )}
-        >
-          <div
-            className={clsx(
-              'NotifiInputSeparator__content',
-              inputSeparators.smsSeparator.classNames?.content,
-            )}
-          >
-            {inputSeparators?.smsSeparator?.content}
-          </div>
-        </div>
-      ) : null}
-      {data.contactInfo.telegram.active ? (
-        <NotifiTelegramInput
-          disabled={inputDisabled}
-          classNames={classNames?.NotifiTelegramInput}
-          copy={{
-            label: inputTextFields?.label?.telegram,
-            placeholder: inputTextFields?.placeholderText?.telegram,
-          }}
-        />
-      ) : null}
-      {inputSeparators?.telegramSeparator?.content ? (
-        <div
-          className={clsx(
-            'NotifiInputSeparator__container',
-            inputSeparators?.telegramSeparator?.classNames?.container,
-          )}
-        >
-          <div
-            className={clsx(
-              'NotifiInputSeparator__content',
-              inputSeparators.telegramSeparator.classNames?.content,
-            )}
-          >
-            {inputSeparators?.telegramSeparator?.content}
-          </div>
-        </div>
-      ) : null}
+      {!hideContactInputs ? (
+        <>
+          {data.contactInfo.email.active ? (
+            <NotifiEmailInput
+              disabled={inputDisabled}
+              classNames={classNames?.NotifiEmailInput}
+              copy={{
+                label: inputTextFields?.label?.email,
+                placeholder: inputTextFields?.placeholderText?.email,
+              }}
+            />
+          ) : null}
+          {inputSeparators?.emailSeparator?.content ? (
+            <div
+              className={clsx(
+                'NotifiInputSeparator__container',
+                inputSeparators?.emailSeparator?.classNames?.container,
+              )}
+            >
+              <div
+                className={clsx(
+                  'NotifiInputSeparator__content',
+                  inputSeparators.emailSeparator.classNames?.content,
+                )}
+              >
+                {inputSeparators?.emailSeparator?.content}
+              </div>
+            </div>
+          ) : null}
+          {data.contactInfo.sms.active ? (
+            <NotifiSmsInput
+              disabled={inputDisabled}
+              classNames={classNames?.NotifiSmsInput}
+              allowedCountryCodes={allowedCountryCodes}
+              copy={{
+                label: inputTextFields?.label?.sms,
+                placeholder: inputTextFields?.placeholderText?.sms,
+              }}
+            />
+          ) : null}
+          {inputSeparators?.smsSeparator?.content ? (
+            <div
+              className={clsx(
+                'NotifiInputSeparator__container',
+                inputSeparators?.smsSeparator?.classNames?.container,
+              )}
+            >
+              <div
+                className={clsx(
+                  'NotifiInputSeparator__content',
+                  inputSeparators.smsSeparator.classNames?.content,
+                )}
+              >
+                {inputSeparators?.smsSeparator?.content}
+              </div>
+            </div>
+          ) : null}
+          {data.contactInfo.telegram.active ? (
+            <NotifiTelegramInput
+              disabled={inputDisabled}
+              classNames={classNames?.NotifiTelegramInput}
+              copy={{
+                label: inputTextFields?.label?.telegram,
+                placeholder: inputTextFields?.placeholderText?.telegram,
+              }}
+            />
+          ) : null}
+          {inputSeparators?.telegramSeparator?.content ? (
+            <div
+              className={clsx(
+                'NotifiInputSeparator__container',
+                inputSeparators?.telegramSeparator?.classNames?.container,
+              )}
+            >
+              <div
+                className={clsx(
+                  'NotifiInputSeparator__content',
+                  inputSeparators.telegramSeparator.classNames?.content,
+                )}
+              >
+                {inputSeparators?.telegramSeparator?.content}
+              </div>
+            </div>
+          ) : null}
 
-      {data.contactInfo?.discord?.active ? (
-        <NotifiDiscordToggle
-          disabled={inputDisabled}
-          classNames={classNames?.NotifiDiscordToggle}
-        />
+          {data.contactInfo?.discord?.active ? (
+            <NotifiDiscordToggle
+              disabled={inputDisabled}
+              classNames={classNames?.NotifiDiscordToggle}
+            />
+          ) : null}
+        </>
       ) : null}
-
       {params.walletBlockchain === 'SOLANA' ? (
         <NotifiHwWalletToggle
           disabled={inputDisabled}
