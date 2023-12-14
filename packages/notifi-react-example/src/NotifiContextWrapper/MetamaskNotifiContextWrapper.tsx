011b3f5c0ccdbd53f2ed7ecbb593144b73fc4c56
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2023-12-14 11:03:11 +0900

    MVP-3803: notifi-react-example EVM multi-chain support (#430)
    * MVP-3803: notifi-react-example EVM multi-chain support
    
    * Remove useEffect
    
    ---------
    
    Co-authored-by: eric-notifi <macbookpro4eric@Mac-mini.local>

--- packages/notifi-react-example/src/NotifiContextWrapper/MetamaskNotifiContextWrapper.tsx
+++ packages/notifi-react-example/src/NotifiContextWrapper/MetamaskNotifiContextWrapper.tsx
@@ -1,8 +1,39 @@
+import { WalletBlockchain } from '@notifi-network/notifi-core';
+import { Uint8SignMessageFunction } from '@notifi-network/notifi-frontend-client';
 import { NotifiContext } from '@notifi-network/notifi-react-card';
 import { arrayify } from 'ethers/lib/utils.js';
-import { PropsWithChildren } from 'react';
-import { useAccount, useConnect, useSignMessage } from 'wagmi';
+import { PropsWithChildren, useMemo } from 'react';
+import {
+  useAccount,
+  useConnect,
+  useNetwork,
+  useSignMessage,
+  useSwitchNetwork,
+} from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
+
+enum SupportedEvmChains {
+  ETHEREUM = 1,
+  BINANCE = 56,
+  AVALANCHE = 43114,
+  ARBITRUM = 42161,
+  POLYGON = 137,
+  OPTIMISM = 10,
+  ZKSYNC = 324,
+}
+
+const supportedEvmChains = [
+  SupportedEvmChains.ETHEREUM,
+  SupportedEvmChains.BINANCE,
+  SupportedEvmChains.AVALANCHE,
+  SupportedEvmChains.ARBITRUM,
+  SupportedEvmChains.POLYGON,
+  SupportedEvmChains.OPTIMISM,
+  SupportedEvmChains.ZKSYNC,
+];
+
+const tenantId = 'junitest.xyz';
+const env = 'Development';
 
 export const MetamaskNotifiContextWrapper: React.FC<PropsWithChildren> = ({
   children,
@@ -13,23 +44,71 @@ export const MetamaskNotifiContextWrapper: React.FC<PropsWithChildren> = ({
     connector: new InjectedConnector(),
   });
 
+  const { chain } = useNetwork();
   const { signMessageAsync } = useSignMessage();
+
+  const { switchNetwork } = useSwitchNetwork();
+
+  const walletBlockchain: WalletBlockchain = useMemo(() => {
+    switch (chain?.id) {
+      case SupportedEvmChains.ETHEREUM:
+        return 'ETHEREUM';
+      case SupportedEvmChains.BINANCE:
+        return 'BINANCE';
+      case SupportedEvmChains.AVALANCHE:
+        return 'AVALANCHE';
+      case SupportedEvmChains.ARBITRUM:
+        return 'ARBITRUM';
+      case SupportedEvmChains.POLYGON:
+        return 'POLYGON';
+      case SupportedEvmChains.OPTIMISM:
+        return 'OPTIMISM';
+      case SupportedEvmChains.ZKSYNC:
+        return 'ZKSYNC';
+      default:
+        return 'ETHEREUM';
+    }
+  }, [chain]);
+
+  const signMessage: Uint8SignMessageFunction = async (message: Uint8Array) => {
+    const result = await signMessageAsync({ message });
+    return arrayify(result);
+  };
+
+  if (!chain?.network) {
+    return (
+      <div>
+        <button onClick={() => connect()} disabled={isConnected}>
+          {isConnected ? `${address}` : 'Connect Wallet'}
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div>
+      <select
+        value={chain.id}
+        onChange={(e) => switchNetwork?.(Number(e?.target?.value) ?? 1)}
+      >
+        {supportedEvmChains.map((chainId) => (
+          <option key={chainId} value={chainId}>
+            {chainId}
+          </option>
+        ))}
+      </select>
+      {chain && <div>Connected Chain: {walletBlockchain}</div>}
       <button onClick={() => connect()} disabled={isConnected}>
         {isConnected ? `${address}` : 'Connect Wallet'}
       </button>
-      {isConnected ? (
+
+      {isConnected && chain?.network ? (
         <NotifiContext
-          dappAddress="junitest.xyz"
-          env="Development"
-          signMessage={async (message: Uint8Array) => {
-            const result = await signMessageAsync({ message });
-            return arrayify(result);
-          }}
+          dappAddress={tenantId}
+          env={env}
+          signMessage={signMessage}
           walletPublicKey={address ?? ''}
-          walletBlockchain="AVALANCHE"
+          walletBlockchain={walletBlockchain}
         >
           {children}
         </NotifiContext>
