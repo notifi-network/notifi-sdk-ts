981423ce4e070d9170b6f38cc9e2492c1a8b0f46
notifi-jamie <jamie.michel@notifi.network>
2023-12-19 23:38:45 +0000

    feat: add docker-compose file and bash script to start local env

---
+++ fusion-development-environment/start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Constants
+NOTIFI_DIRECTORY="$HOME/.notifi"
+DEVELOPMENT_ENVIRONMENT_DIRECTORY="$NOTIFI_DIRECTORY/fusion-development"
+CREDENTIALS_DIRECTORY="$NOTIFI_DIRECTORY/credentials"
+FUSION_SOURCES_DIRECTORY="$DEVELOPMENT_ENVIRONMENT_DIRECTORY/fusion-sources"
+
+DEV_AUTH_TOKEN_FILE="$CREDENTIALS_DIRECTORY/dev-token.txt"
+DOCKER_COMPOSE_FILE_LOCATION="$DEVELOPMENT_ENVIRONMENT_DIRECTORY/docker-compose.yml"
+DOCKER_COMPOSE_DOWNLOAD_URL=""
+
+# Step 1: Setup the required directories on host machine
+
+# Array of directories to create
+directories=($NOTIFI_DIRECTORY $DEVELOPMENT_ENVIRONMENT_DIRECTORY $CREDENTIALS_DIRECTORY $FUSION_SOURCES_DIRECTORY)
+
+# Loop through the array of required directories and create if they don't exist
+for dir in "${directories[@]}"; do
+    if [[ ! -d $dir ]]; then
+        mkdir -p "$dir"
+    fi
+done
+
+if [[ ! -f $DEV_AUTH_TOKEN_FILE ]]; then        
+    touch "$DEV_AUTH_TOKEN_FILE"
+fi
+
+# Step 2: Download the latest version of the docker-compose file
+# curl -o $DOCKER_COMPOSE_FILE_LOCATION $DOCKER_COMPOSE_DOWNLOAD_URL
+
+# Step 3: Export the required environment variables
+export HOST_USER_ID=$(id -u)
+export HOST_GROUP_ID=$(id -g)
+
+
+# Step 4: Run the docker-compose file
+
+# Stop existing containers, if any
+docker-compose -f $DOCKER_COMPOSE_FILE_LOCATION down || true
+
+# Run Docker Compose
+docker-compose -f $DOCKER_COMPOSE_FILE_LOCATION up localfusion --build -d
+docker-compose -f $DOCKER_COMPOSE_FILE_LOCATION up --build -d fusion-development
+
+# Display the latest logs and then continue
+docker-compose -f $DOCKER_COMPOSE_FILE_LOCATION logs fusion-development
+# Attach to the container for interaction
+docker attach fusion-development
