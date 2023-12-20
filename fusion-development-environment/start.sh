d74507081e6ae7f8e7680ecf3f85cd2a15c1ca37
notifi-jamie <jamie.michel@notifi.network>
2023-12-20 00:21:11 +0000

    feat: add temporary download url

--- fusion-development-environment/start.sh
+++ fusion-development-environment/start.sh
@@ -8,7 +8,7 @@ FUSION_SOURCES_DIRECTORY="$DEVELOPMENT_ENVIRONMENT_DIRECTORY/fusion-sources"
 
 DEV_AUTH_TOKEN_FILE="$CREDENTIALS_DIRECTORY/dev-token.txt"
 DOCKER_COMPOSE_FILE_LOCATION="$DEVELOPMENT_ENVIRONMENT_DIRECTORY/docker-compose.yml"
-DOCKER_COMPOSE_DOWNLOAD_URL=""
+DOCKER_COMPOSE_DOWNLOAD_URL="https://raw.githubusercontent.com/notifi-network/notifi-sdk-ts/jm/local-devenv-files/fusion-development-environment/docker-compose.yml" #TODO: update this once merged to main
 
 # Step 1: Setup the required directories on host machine
 
@@ -27,7 +27,7 @@ if [[ ! -f $DEV_AUTH_TOKEN_FILE ]]; then
 fi
 
 # Step 2: Download the latest version of the docker-compose file
-# curl -o $DOCKER_COMPOSE_FILE_LOCATION $DOCKER_COMPOSE_DOWNLOAD_URL
+curl -fsSL -o $DOCKER_COMPOSE_FILE_LOCATION $DOCKER_COMPOSE_DOWNLOAD_URL
 
 # Step 3: Export the required environment variables
 export HOST_USER_ID=$(id -u)
