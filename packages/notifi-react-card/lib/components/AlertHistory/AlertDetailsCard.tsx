5019dbe4f19348743c7974761a3aff07ac71358d
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2023-12-20 17:48:32 +0900

    MVP-3643: new cardView historyDetail (#431)
    * MVP-3643: new cardView historyDetail
    
    * Fix double header when loading history
    
    * Remove deprecated className override
    
    ---------
    
    Co-authored-by: eric-notifi <macbookpro4eric@Mac-mini.local>

--- packages/notifi-react-card/lib/components/AlertHistory/AlertDetailsCard.tsx
+++ packages/notifi-react-card/lib/components/AlertHistory/AlertDetailsCard.tsx
@@ -1,9 +1,14 @@
 import clsx from 'clsx';
 import DOMPurify from 'dompurify';
+import { useNotifiSubscriptionContext } from 'notifi-react-card/lib/context';
 import { getAlertDetailsContents } from 'notifi-react-card/lib/utils';
 import React, { useMemo } from 'react';
 
 import { formatAlertDetailsTimestamp } from '../../utils/AlertHistoryUtils';
+import NotifiAlertBox, {
+  NotifiAlertBoxButtonProps,
+  NotifiAlertBoxProps,
+} from '../NotifiAlertBox';
 import { NotificationHistoryEntry } from '../subscription';
 
 export type AlertDetailsProps = Readonly<{
@@ -11,12 +16,20 @@ export type AlertDetailsProps = Readonly<{
   classNames?: Readonly<{
     detailsContainer?: string;
     BackArrowIcon?: string;
+    NotifiAlertBox?: NotifiAlertBoxProps['classNames'];
+    dividerLine?: string;
   }>;
+  headerTitle: string;
+  headerRightIcon?: NotifiAlertBoxButtonProps;
+  onClose?: () => void;
 }>;
 export const AlertDetailsCard: React.FC<AlertDetailsProps> = ({
   notificationEntry,
   classNames,
+  headerTitle,
+  headerRightIcon,
 }) => {
+  const { setCardView } = useNotifiSubscriptionContext();
   const { bottomContent, otherContent, topContent, bottomContentHtml } =
     useMemo(
       () => getAlertDetailsContents(notificationEntry),
@@ -30,31 +43,46 @@ export const AlertDetailsCard: React.FC<AlertDetailsProps> = ({
   }, [bottomContentHtml]);
 
   return (
-    <div
-      className={clsx(
-        'NotifiAlertDetails__container',
-        classNames?.detailsContainer,
-      )}
-    >
-      <div className={clsx('NotifiAlertDetails__topContentContainer')}>
-        <div className={clsx('NotifiAlertDetails__topContent')}>
-          {topContent}
-        </div>
-        <div className={clsx('NotifiAlertDetails__timestamp')}>
-          {formatAlertDetailsTimestamp(notificationEntry.createdDate)}
-        </div>
-      </div>
-      <div className={clsx('NotifiAlertDetails__bottomContent')}>
-        {sanitizedBottomContentHtml ? (
-          // If `messageHtml` exists just use it, otherwise use `message` (which is plain text)
-          <div
-            dangerouslySetInnerHTML={{ __html: sanitizedBottomContentHtml }}
-          />
-        ) : (
-          <div>{bottomContent}</div>
+    <>
+      <NotifiAlertBox
+        classNames={classNames?.NotifiAlertBox}
+        leftIcon={{
+          name: 'back',
+          onClick: () => setCardView({ state: 'history' }),
+        }}
+        rightIcon={headerRightIcon}
+      >
+        <h2>{headerTitle}</h2>
+      </NotifiAlertBox>
+      <div
+        className={clsx(
+          'NotifiAlertDetails__container',
+          classNames?.detailsContainer,
         )}
-        <div>{otherContent}</div>
+      >
+        <div
+          className={clsx('DividerLine historyDetail', classNames?.dividerLine)}
+        />
+        <div className={clsx('NotifiAlertDetails__topContentContainer')}>
+          <div className={clsx('NotifiAlertDetails__topContent')}>
+            {topContent}
+          </div>
+          <div className={clsx('NotifiAlertDetails__timestamp')}>
+            {formatAlertDetailsTimestamp(notificationEntry.createdDate)}
+          </div>
+        </div>
+        <div className={clsx('NotifiAlertDetails__bottomContent')}>
+          {sanitizedBottomContentHtml ? (
+            // If `messageHtml` exists just use it, otherwise use `message` (which is plain text)
+            <div
+              dangerouslySetInnerHTML={{ __html: sanitizedBottomContentHtml }}
+            />
+          ) : (
+            <div>{bottomContent}</div>
+          )}
+          <div>{otherContent}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
