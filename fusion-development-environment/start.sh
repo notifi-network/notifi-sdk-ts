b2c5b8e62525d6ebd31462d58b837865b102d5fb
notifi-jamie <jamie.michel@notifi.network>
2023-12-20 00:27:48 +0000

    feat: use main branch url

--- fusion-development-environment/start.sh
+++ fusion-development-environment/start.sh
@@ -8,7 +8,8 @@ FUSION_SOURCES_DIRECTORY="$DEVELOPMENT_ENVIRONMENT_DIRECTORY/fusion-sources"
 
 DEV_AUTH_TOKEN_FILE="$CREDENTIALS_DIRECTORY/dev-token.txt"
 DOCKER_COMPOSE_FILE_LOCATION="$DEVELOPMENT_ENVIRONMENT_DIRECTORY/docker-compose.yml"
-DOCKER_COMPOSE_DOWNLOAD_URL="https://raw.githubusercontent.com/notifi-network/notifi-sdk-ts/jm/local-devenv-files/fusion-development-environment/docker-compose.yml" #TODO: update this once merged to main
+
+DOCKER_COMPOSE_DOWNLOAD_URL="https://raw.githubusercontent.com/notifi-network/notifi-sdk-ts/main/fusion-development-environment/docker-compose.yml"
 
 # Step 1: Setup the required directories on host machine
 
