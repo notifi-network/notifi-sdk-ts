a4c1123b4f6c47c77b3da07786e1f950024b19cf
eric-notifi <macbookpro4eric@Mac-mini.local>
2023-12-22 10:38:21 +0900

    SDK3.0/MVP-3649: Refactor cardView expired

--- packages/notifi-react-card/lib/components/subscription/subscription-card-views/ExpiredTokenViewCard.tsx
+++ packages/notifi-react-card/lib/components/subscription/subscription-card-views/ExpiredTokenViewCard.tsx
@@ -1,4 +1,3 @@
-import { SignMessageParams } from '@notifi-network/notifi-frontend-client';
 import clsx from 'clsx';
 import {
   useNotifiClientContext,
@@ -8,19 +7,35 @@ import React from 'react';
 
 import { useNotifiSubscribe } from '../../../hooks';
 import { useFrontendClientLogin } from '../../../hooks/useFrontendClientLogin';
+import NotifiAlertBox, {
+  NotifiAlertBoxButtonProps,
+  NotifiAlertBoxProps,
+} from '../../NotifiAlertBox';
 
 export type ExpiredTokenViewCardProps = {
   classNames?: {
+    NotifiAlertBox?: NotifiAlertBoxProps['classNames'];
+    dividerLine?: string;
     container?: string;
     title?: string;
     subtitle?: string;
     button?: string;
     buttonLabel?: string;
   };
+  copy?: {
+    contentTitle?: string;
+    contentBody?: string;
+    buttonText?: string;
+  };
+  headerRightIcon?: NotifiAlertBoxButtonProps;
+  headerTitle?: string;
 };
 
 export const ExpiredTokenView: React.FC<ExpiredTokenViewCardProps> = ({
   classNames,
+  headerRightIcon,
+  headerTitle,
+  copy,
 }) => {
   const { logIn } = useNotifiSubscribe({ targetGroupName: 'Default' });
   const { isUsingFrontendClient } = useNotifiClientContext();
@@ -43,37 +58,48 @@ export const ExpiredTokenView: React.FC<ExpiredTokenViewCardProps> = ({
   };
 
   return (
-    <div
-      className={clsx(
-        'NotifiExpiredTokenCard__container',
-        classNames?.container,
-      )}
-    >
-      <div className={clsx('NotifiExpiredTokenCard__title', classNames?.title)}>
-        Connect to Notifi
-      </div>
+    <>
+      <NotifiAlertBox
+        classNames={classNames?.NotifiAlertBox}
+        rightIcon={headerRightIcon}
+      >
+        <h2>{headerTitle}</h2>
+      </NotifiAlertBox>
+      <div className={clsx('DividerLine expired', classNames?.dividerLine)} />
       <div
         className={clsx(
-          'NotifiExpiredTokenCard__subtitle',
-          classNames?.subtitle,
+          'NotifiExpiredTokenCard__container',
+          classNames?.container,
         )}
       >
-        It's been a while! Connect to Notifi to load your <br />
-        notification details.
-      </div>
-      <button
-        className={clsx('NotifiExpiredTokenCard__button', classNames?.button)}
-        onClick={handleClick}
-      >
-        <span
+        <div
+          className={clsx('NotifiExpiredTokenCard__title', classNames?.title)}
+        >
+          {copy?.contentTitle ?? 'Connect to Notifi'}
+        </div>
+        <div
           className={clsx(
-            'NotifiExpiredTokenCard__buttonLabel',
-            classNames?.buttonLabel,
+            'NotifiExpiredTokenCard__subtitle',
+            classNames?.subtitle,
           )}
         >
-          Connect to Notifi
-        </span>
-      </button>
-    </div>
+          {copy?.contentBody ??
+            "It's been a while! Connect to Notifi to load your notification details."}
+        </div>
+        <button
+          className={clsx('NotifiExpiredTokenCard__button', classNames?.button)}
+          onClick={handleClick}
+        >
+          <span
+            className={clsx(
+              'NotifiExpiredTokenCard__buttonLabel',
+              classNames?.buttonLabel,
+            )}
+          >
+            {copy?.buttonText ?? 'Connect to Notifi'}
+          </span>
+        </button>
+      </div>
+    </>
   );
 };
