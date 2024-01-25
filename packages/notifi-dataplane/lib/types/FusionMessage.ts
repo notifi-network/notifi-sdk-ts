cf46350dd502f3927f8a5f8dadc81af81a9cfe41
hans-notifi <hans.jorgensen@notifi.network>
2024-01-25 14:12:12 -0800

    Change specificWallets to match the shape of the dataplane API

--- packages/notifi-dataplane/lib/types/FusionMessage.ts
+++ packages/notifi-dataplane/lib/types/FusionMessage.ts
@@ -3,24 +3,21 @@ export interface FusionMessage {
   variablesJson: any;
   specificWallets?: ReadonlyArray<
     Readonly<{
-      walletPublicKey: string;
-      // NOTE: Blockchain duplicated here because there would be a circular reference otherwise
-      walletBlockchain:
-        | 'SOLANA'
-        | 'ETHEREUM'
-        | 'AVALANCHE'
-        | 'APTOS'
-        | 'ACALA'
-        | 'POLYGON'
-        | 'ARBITRUM'
-        | 'BINANCE'
-        | 'NEAR'
-        | 'OPTIMISM'
-        | 'INJECTIVE'
-        | 'OSMOSIS'
-        | 'NIBIRU'
-        | 'SUI'
-        | 'ZKSYNC';
+      [walletAddress in string]: | 'SOLANA'
+      | 'ETHEREUM'
+      | 'AVALANCHE'
+      | 'APTOS'
+      | 'ACALA'
+      | 'POLYGON'
+      | 'ARBITRUM'
+      | 'BINANCE'
+      | 'NEAR'
+      | 'OPTIMISM'
+      | 'INJECTIVE'
+      | 'OSMOSIS'
+      | 'NIBIRU'
+      | 'SUI'
+      | 'ZKSYNC'
     }>
   >;
 }
