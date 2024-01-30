a5afdbb9a8810b04e8f443a8b850eb1e32fa0090
eric-notifi <eric.lee@notifi.network>
2024-01-30 10:28:11 +0900

    Customize config to allow dark/light mode

--- packages/notifi-dapp-example/src/app/notifi/page.tsx
+++ packages/notifi-dapp-example/src/app/notifi/page.tsx
@@ -1,6 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-injective">
+    <main
+      // TODO: depends on NotifiContext
+      dark-mode="false"
+      className="notifi-dapp flex min-h-screen flex-col items-center justify-between p-24 bg-notifi-page-bg"
+    >
       <div className="text-gray-950">We’re excited, start from here</div>
     </main>
   );
--- packages/notifi-dapp-example/tailwind.config.ts
+++ packages/notifi-dapp-example/tailwind.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  purge: {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      safelist: ['html', 'body'],
+    },
+  },
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,12 +16,13 @@ const config: Config = {
     fontFamily: {
       rota: ['var(--font-rota)'],
     },
+    colors: {
+      // TODO: Temp bg color. TBD
+      'notifi-page-bg': 'rgb(var(--notifi-page-bg))',
+    },
     extend: {
       backgroundImage: {
-        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        // 'gradient-conic':
-        //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'gradient-injective': `linear-gradient(107deg, rgba(238, 243, 250, 0.50) 1.37%, rgba(225, 235, 248, 0.50) 47.55%, rgba(198, 233, 245, 0.50) 103.36%)`,
+        'gradient-notifi': `linear-gradient(107deg, rgb(var(--notifi-bgimg-left)) 1.37%, rgb(var(--notifi-bgimg-middle)) 47.55%, rgb(var(--notifi-bgimg-right)) 103.36%)`,
       },
     },
   },
