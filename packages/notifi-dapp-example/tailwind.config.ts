c409b3d941f649c450fa7a05d0e7d7fb1dca7005
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-01-31 09:37:23 +0900

    OpenDefi/MVP-3954: Build up infra for new page `notifi-dapp-example` (#461)
    * dapp-example/MVP-3954: nexjs & tailwindcss project infra
    
    * Customize config to allow dark/light mode

---
+++ packages/notifi-dapp-example/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  purge: {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      safelist: ['html', 'body'],
+    },
+  },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      rota: ['var(--font-rota)'],
+    },
+    colors: {
+      // TODO: Temp bg color. TBD
+      'notifi-page-bg': 'rgb(var(--notifi-page-bg))',
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-notifi': `linear-gradient(107deg, rgb(var(--notifi-bgimg-left)) 1.37%, rgb(var(--notifi-bgimg-middle)) 47.55%, rgb(var(--notifi-bgimg-right)) 103.36%)`,
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
