baf47033f05506ce516083416c81cda53f7c673a
Hao Wang <ge_nah@hotmail.com>
2024-02-21 00:02:39 -0800

    removed unused fragment

--- packages/notifi-dapp-example/src/app/notifi/ftu/page.tsx
+++ packages/notifi-dapp-example/src/app/notifi/ftu/page.tsx
@@ -44,19 +44,17 @@ export default function NotifiFTU() {
     return null;
   }
   return (
-    <>
-      <div className="w-full h-screen flex flex-col items-center justify-start bg-notifi-page-bg">
-        <DappIcon />
-        <div className="text-blue-500 font-bold text-lg mb-8">
-          Injective ecosystem alerts
-        </div>
-        {ftuStage === FtuStage.Destination ? (
-          <ConfigDestinationModal contactInfo={cardConfig.contactInfo} />
-        ) : null}
-
-        {ftuStage === FtuStage.Alerts ? <ConfigAlertModal /> : null}
-        <PoweredByNotifi />
+    <div className="w-full h-screen flex flex-col items-center justify-start bg-notifi-page-bg">
+      <DappIcon />
+      <div className="text-blue-500 font-bold text-lg mb-8">
+        Injective ecosystem alerts
       </div>
-    </>
+      {ftuStage === FtuStage.Destination ? (
+        <ConfigDestinationModal contactInfo={cardConfig.contactInfo} />
+      ) : null}
+
+      {ftuStage === FtuStage.Alerts ? <ConfigAlertModal /> : null}
+      <PoweredByNotifi />
+    </div>
   );
 }
