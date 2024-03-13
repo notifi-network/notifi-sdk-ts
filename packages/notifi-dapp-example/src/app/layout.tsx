cc9fdd2aca613bfe1186c20c5ba006aa91de921e
eric-notifi <macbookpro4eric@Mac-mini.local>
2024-03-13 17:30:18 +0900

    MVP-4227-2: chore: clean up dependency npm packages

--- packages/notifi-dapp-example/src/app/layout.tsx
+++ packages/notifi-dapp-example/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { GlobalStateContextProvider } from '@/context/GlobalStateContext';
 import { NotifiWalletsWrapper } from '@/context/NotifiWalletsWrapper';
-import '@interchain-ui/react/styles';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
