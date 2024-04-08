9ab5f7feb2f8a4f087fc46c33645fe4038bbc5fc
eric-notifi <eric.lee@notifi.network>
2024-04-08 17:24:17 +0900

    MVP-4329: Define the interface for fusion Event Metadata

---
+++ packages/notifi-frontend-client/lib/models/FusionEvent.ts
@@ -0,0 +1,53 @@
+export type FusionEventMetadata = {
+  uiConfigOverride?: {
+    topicDisplayName?: string;
+    historyDisplayName?: string;
+  };
+  filters: Array<AlertFilter>;
+  requiredParserVariables: Array<RequiredParserVariable>;
+};
+
+/**
+ * @param name - `string` unique name
+ */
+export type AlertFilter = {
+  name: string;
+  type: AlertFilterType;
+  executionPriority: number;
+  minimumDurationBetweenTriggersInMinutes: number;
+  userInputParams: UserInputParam<UiType>[];
+  staticFilterParams?: Record<string, object | string | number>;
+};
+
+export type RequiredParserVariable = {
+  variableName: string;
+  variableType: ValueType;
+  variableDescription: string;
+};
+
+export type ValueType = 'integer' | 'price' | 'percentage';
+
+/**
+ * @param UiType - `radio` or `button` (scalable). Define what component should be rendered in Card topic subscription view.
+ * @param defaultValue - The value for default alert subscription
+ */
+export type UserInputParam<T extends UiType> = {
+  name: string;
+  kind: T;
+  valueTypes?: ValueType;
+  options: (string | number)[];
+  defaultValue: string | number;
+  allowCustomInput?: boolean;
+};
+
+export type UiType = 'radio' | 'button';
+type AlertFilterType = 'alertFilter';
+
+export type FusionFilterOptions = {
+  input: Record<AlertFilter['name'], UserInputOptions>;
+};
+
+export type UserInputOptions = Record<
+  UserInputParam<UiType>['name'],
+  UserInputParam<UiType>['options'][number]
+>;
