9a0e26c0fb92f3e3f61617c582ed024bcc7d31ac
eric-notifi <eric.lee@notifi.network>
2024-04-10 23:22:18 +0900

    MVP-4376: Connect to notifi service before clicking signup button

--- packages/notifi-dapp-example/src/context/NotifiUserSettingContext.tsx
+++ packages/notifi-dapp-example/src/context/NotifiUserSettingContext.tsx
@@ -9,7 +9,6 @@ import {
 } from 'react';
 
 import { useNotifiFrontendClientContext } from './NotifiFrontendClientContext';
-import { useNotifiTenantConfig } from './NotifiTenantConfigContext';
 
 export enum FtuStage {
   Destination = 3,
@@ -31,20 +30,13 @@ export const NotifiUserSettingContextProvider: FC<PropsWithChildren> = ({
 }) => {
   const { frontendClientStatus, frontendClient } =
     useNotifiFrontendClientContext();
-  const { cardConfig } = useNotifiTenantConfig();
 
   const [ftuStage, setFtuStage] = useState<FtuStage | null>(null);
 
   useEffect(() => {
     if (!frontendClientStatus.isAuthenticated) return;
     frontendClient.getUserSettings().then((userSettings) => {
-      if (!userSettings?.ftuStage) {
-        if (cardConfig?.isContactInfoRequired) {
-          return updateFtuStage(FtuStage.Destination);
-        }
-        return updateFtuStage(FtuStage.Alerts);
-      }
-      setFtuStage(userSettings.ftuStage);
+      setFtuStage(userSettings?.ftuStage ?? null);
     });
   }, [frontendClientStatus.isAuthenticated]);
 
