17ffbfc15c6a97b011a524202a0fa178606a2867
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-04-18 15:57:15 +0800

    notifi-react/MVP-4342(pre-mvp-4343): finalaize context (#534)
    * MVP-4342-patch: finalaize context
    
    * MVP-4342-patch2: Add Change listener & bug fix into NotifiTargetContext
    
    * MVP-4342-patch3: Add fusionEventDescriptor types in NotifiTopicContext

---
+++ packages/notifi-react/lib/context/GlobalStateContext.tsx
@@ -0,0 +1,68 @@
+import React, {
+  Dispatch,
+  FC,
+  PropsWithChildren,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from 'react';
+
+export type GlobalLoading = {
+  isLoading: boolean;
+  loadingData?: string; // Allowing to pass custom string data to be parsed by JSON.parse
+};
+
+export type GlobalError = {
+  error: Error;
+  errorData?: string; // Allowing to pass custom string data to be parsed by JSON.parse
+};
+
+type GlobalCtas = Record<string, () => void>;
+
+export type GlobalStateContextType = {
+  globalLoading: GlobalLoading;
+  setGlobalLoading: Dispatch<SetStateAction<GlobalLoading>>;
+  globalError: GlobalError | null;
+  setGlobalError: Dispatch<SetStateAction<GlobalError | null>>;
+  globalCtas: GlobalCtas | null;
+  setGlobalCtas: Dispatch<SetStateAction<GlobalCtas | null>>;
+};
+
+const GlobalStateContext = createContext<GlobalStateContextType>({
+  globalLoading: { isLoading: false },
+  setGlobalLoading: () => undefined,
+  globalError: null,
+  setGlobalError: () => undefined,
+  globalCtas: null,
+  setGlobalCtas: () => undefined,
+});
+
+export const GlobalStateContextProvider: FC<PropsWithChildren> = ({
+  children,
+}) => {
+  const [globalLoading, setGlobalLoading] = useState<GlobalLoading>({
+    isLoading: false,
+  });
+
+  const [globalError, setGlobalError] = useState<GlobalError | null>(null);
+
+  const [globalCtas, setGlobalCtas] = useState<GlobalCtas | null>(null);
+
+  return (
+    <GlobalStateContext.Provider
+      value={{
+        globalLoading,
+        setGlobalLoading,
+        globalError,
+        setGlobalError,
+        globalCtas,
+        setGlobalCtas,
+      }}
+    >
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+export const useGlobalStateContext = () => useContext(GlobalStateContext);
