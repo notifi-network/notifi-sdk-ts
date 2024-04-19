2c362b555ab319255b3f6b4e711022a8af294f15
Vaishali <Vaishali.Jain@notify.Network>
2024-04-19 21:17:58 +0530

    remove retry functionality as Graphql-ws itself supporting

--- packages/notifi-graphql/lib/NotifiSubscriptionService.ts
+++ packages/notifi-graphql/lib/NotifiSubscriptionService.ts
@@ -1,7 +1,6 @@
 import { createClient, Client } from 'graphql-ws';
 export class NotifiSubscriptionService {
   private wsClient: Client | undefined;
-  private retryCount = 0;
   private jwt: string | undefined;
   constructor(private wsurl: string) {
   }
@@ -49,49 +48,12 @@ export class NotifiSubscriptionService {
   }
 
   private initializeClient = () => {
-    //transport
     this.wsClient = createClient({
       url: this.wsurl,
       connectionParams: {
         Authorization: `Bearer ${this.jwt}`,
       },
-      lazyCloseTimeout: 3000,
-      disablePong: true
     });
-
-    this.wsClient.on('connected', () => {
-      this.wsStatus = 'Connected';
-    });
-
-    this.wsClient.on('error', (error) => {
-      this.wsStatus = 'Failed';
-    });
-
-    this.wsClient.on('closed', () => {
-      
-    });
-  }
-
-  private handleOnError() {
-    this.wsClient?.terminate();
-    this.wsClient?.dispose();
-    this.initializeClient()
-  }
-
-  public onRetry() {
-    if (this.retryCount < 3) {
-      setTimeout(() => {
-        this.handleOnError();
-        this.retryCount++;
-        if (this.wsStatus !== 'Connected' && this.retryCount < 3) {
-          this.onRetry();
-        }
-      }, 3000);
-    } else {
-      if (this.retryCount >= 3 && this.wsStatus !== 'Connected') {
-        console.log('Not able to connect, please contact to admin...');
-      }
-    }
   }
 }
 
