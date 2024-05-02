dfbbc113e6a10a32ef27c5a131e494c92c86411c
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-05-02 22:25:35 +0900

    notifi-react/MVP-4343: notifi-react NotifiCardModal FTU flow (#543)
    * MVP-4343-1: scaffolding card-modal basic components (#538)
    
    * MVP-4343-2: add Connect.tsx
    
    * MVP-4343-3: FtuAlertList
    
    * MVP-4343-4: FtuTargetEdit
    
    * MVP-4343-5: FtuTargetList & TargetList
    
    * MVP-4343-6: FtuAlertEdit & TopicList

---
+++ packages/notifi-react/lib/utils/constants.ts
@@ -0,0 +1,61 @@
+export const defaultCopy = {
+  connect: {
+    title: 'Connect your wallet to use notifications',
+    content: '',
+    buttonText: 'Connect to Wallet',
+  },
+  expiry: {
+    title: 'Subscription Expiry',
+    content: 'Your subscription has expired',
+    buttonText: 'Connect to Wallet',
+  },
+  ftuAlertList: {
+    title: 'Notifications',
+    description:
+      'See your activity and gain access to alerts. Push notifications are also available and optional.',
+    buttonText: 'Next',
+  },
+  poweredByNotifi: 'Powered',
+  inputFields: {
+    email: 'Email address',
+    phoneNumber: 'Phone Number',
+    telegram: 'Telegram ID',
+  },
+  inputToggles: {
+    discord: 'Discord DM Bot',
+    slack: 'Slack',
+  },
+  ftu: {
+    headerTitles: {
+      ftuTargetEdit: 'How do you want to be notified?',
+      ftuAlertEdit: 'Select alerts',
+    },
+  },
+  ftuTargetEdit: {
+    description: 'Select a minimum of one destination',
+    headerTitle: 'How do you want to be notified?',
+    buttonText: 'Next',
+  },
+  ftuTargetList: {
+    headerTitle: 'Verify your destinations',
+    buttonText: 'Next',
+  },
+  ftuAlertEdit: {
+    headerTitle: 'Select alerts',
+    buttonText: 'Done',
+  },
+  targetList: {
+    email: 'Email',
+    phoneNumber: 'SMS',
+    telegram: 'Telegram',
+    discord: 'Discord',
+    slack: 'Slack',
+  },
+  topicOptions: {
+    customInputPlaceholder: 'Custom',
+  },
+};
+
+export const validationRegex: Record<string, RegExp> = {
+  email: new RegExp('^[a-zA-Z0-9._:$!%-+]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'),
+};
