c1969ce2842a3cdde8c0247419dae8f007a09487
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-05-07 22:45:29 +0900

    notifi-react/MVP-4529 (#553)
    * MVP-4529:  topic group:  New component: TopicGroupOptions
    
    * MVP-4528-patch: consolidate topicGroupRow & topicRow/ some refactor

---
+++ packages/notifi-react/lib/hooks/useUserInputParmToFilterOption.tsx
@@ -0,0 +1,79 @@
+import {
+  FusionEventTopic,
+  UiType,
+  UserInputParam,
+} from '@notifi-network/notifi-frontend-client';
+import React from 'react';
+
+import { useNotifiTargetContext, useNotifiTopicContext } from '../context';
+import {
+  derivePrefixAndSuffixFromValueType,
+  getUpdatedAlertFilterOptions,
+} from '../utils';
+
+export const useUserInputParmToFilterOption = (
+  topicName: string,
+  userInputParam: UserInputParam<UiType>,
+) => {
+  const { getAlertFilterOptions, subscribeAlertsWithFilterOptions } =
+    useNotifiTopicContext();
+  const {
+    targetDocument: { targetGroupId },
+  } = useNotifiTargetContext();
+  const alertFilterOptions = getAlertFilterOptions(topicName);
+
+  const filterName: string | undefined = Object.keys(
+    alertFilterOptions?.input ?? [],
+  )[0]; // NOTE: Only consider 1 to 1 relationship (1 filter for 1 topic)
+  const userInputOptions = alertFilterOptions?.input[filterName];
+
+  const subscribedValue = userInputOptions?.[userInputParam.name];
+
+  const [customInput, setCustomInput] = React.useState<string | number>();
+  React.useEffect(() => {
+    if (
+      !!subscribedValue &&
+      !userInputParam.options.includes(subscribedValue)
+    ) {
+      setCustomInput(subscribedValue);
+      return;
+    }
+    setCustomInput('');
+  }, [subscribedValue]);
+
+  const uiType = userInputParam.uiType;
+
+  const prefixAndSuffix = derivePrefixAndSuffixFromValueType(
+    userInputParam.kind,
+  );
+
+  const renewFilterOptions = async (
+    option: number | string,
+    topics: FusionEventTopic[],
+  ) => {
+    if (!alertFilterOptions || !targetGroupId) return;
+    const updatedAlertFilterOptiopns = getUpdatedAlertFilterOptions(
+      filterName,
+      alertFilterOptions,
+      userInputParam.name,
+      option,
+    );
+    await subscribeAlertsWithFilterOptions(
+      topics.map((topic) => {
+        return {
+          topic: topic,
+          filterOptions: updatedAlertFilterOptiopns,
+        };
+      }),
+      targetGroupId,
+    );
+  };
+  return {
+    customInput,
+    prefixAndSuffix,
+    renewFilterOptions,
+    subscribedValue,
+    uiType,
+    setCustomInput,
+  };
+};
