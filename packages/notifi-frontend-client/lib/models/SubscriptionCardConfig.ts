b9f3cadddd9268419ce4fee47494636ebffa831a
Hao Wang <ge_nah@hotmail.com>
2024-05-08 23:45:46 -0700

    fix subscribe to health check event

--- packages/notifi-frontend-client/lib/models/SubscriptionCardConfig.ts
+++ packages/notifi-frontend-client/lib/models/SubscriptionCardConfig.ts
@@ -34,6 +34,7 @@ export type FusionTypeBase = {
   alertFrequency?: AlertFrequency;
   optOutAtSignup?: boolean;
   displayNameOverride?: string;
+  topicGroupName?: string;
 };
 
 export type FusionToggleEventTypeItem = FusionTypeBase &
