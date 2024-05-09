d338c195548e520efda4a12473adc3f8fa184cd6
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-05-09 13:02:11 +0900

    MVP-4531+4552: (GMX-related) TopicList & SubscriptionValueInput component (#556)
    * MVP-4531: TopicList & functional test & Style
    
    * MVP-4552: SubscriptionValueInput

--- packages/notifi-react/lib/components/TopicList.tsx
+++ packages/notifi-react/lib/components/TopicList.tsx
@@ -1,3 +1,4 @@
+import { FusionEventTopic } from '@notifi-network/notifi-frontend-client';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -10,13 +11,79 @@ export type TopicListProps = {
   };
 };
 
+export type TopicRowCategory = 'standalone' | 'group';
+
+export type TopicRowMetadata<T extends TopicRowCategory> =
+  T extends 'standalone' ? TopicStandaloneRowMetadata : TopicGroupRowMetadata;
+
+type TopicRowMetadataBase = {
+  index: number;
+};
+
+export type TopicStandaloneRowMetadata = TopicRowMetadataBase & {
+  topic: FusionEventTopic;
+};
+
+export type TopicGroupRowMetadata = TopicRowMetadataBase & {
+  topicGroupName: string;
+  topics: FusionEventTopic[];
+};
+
 export const TopicList: React.FC<TopicListProps> = (props) => {
   const { fusionEventTopics } = useNotifiTenantConfigContext();
+
+  // TODO: Move this to a hook
+  const topicRows = React.useMemo(() => {
+    const fusionEventTopicGroups: Record<string, TopicGroupRowMetadata> = {};
+    const fusionEventStandaloneTopics: TopicStandaloneRowMetadata[] = [];
+    fusionEventTopics.forEach((topic, index) => {
+      if (topic.uiConfig.topicGroupName) {
+        if (!fusionEventTopicGroups[topic.uiConfig.topicGroupName]) {
+          fusionEventTopicGroups[topic.uiConfig.topicGroupName] = {
+            index,
+            topicGroupName: topic.uiConfig.topicGroupName,
+            topics: [topic],
+          };
+        } else {
+          fusionEventTopicGroups[topic.uiConfig.topicGroupName].topics.push(
+            topic,
+          );
+        }
+        return;
+      }
+      fusionEventStandaloneTopics.push({ topic, index });
+    });
+
+    return [
+      ...Object.values(fusionEventTopicGroups),
+      ...fusionEventStandaloneTopics,
+    ].sort((a, b) => a.index - b.index);
+  }, [fusionEventTopics]);
+
   return (
     <div className={clsx('notifi-topic-list', props.classNames?.container)}>
-      {fusionEventTopics.map((topic) => (
-        <TopicRow topic={topic} key={topic.uiConfig.name} />
-      ))}
+      {topicRows.map((rowMetadata, id) => {
+        if (isTopicStandaloneMetadata(rowMetadata)) {
+          return <TopicRow<'standalone'> {...rowMetadata} key={id} />;
+        }
+        if (isTopicGroupMetadata(rowMetadata)) {
+          return <TopicRow<'group'> key={id} {...rowMetadata} />;
+        }
+      })}
     </div>
   );
+};
+
+// Utils
+
+const isTopicStandaloneMetadata = (
+  topicRowMetadata: TopicRowMetadata<TopicRowCategory>,
+): topicRowMetadata is TopicStandaloneRowMetadata => {
+  return 'topic' in topicRowMetadata;
+};
+
+const isTopicGroupMetadata = (
+  topicRowMetadata: TopicRowMetadata<TopicRowCategory>,
+): topicRowMetadata is TopicGroupRowMetadata => {
+  return 'topicGroupName' in topicRowMetadata && 'topics' in topicRowMetadata;
 };
