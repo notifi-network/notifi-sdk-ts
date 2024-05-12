44bf4de2c588ccd79c2abaa8f4e69c8212e7ff83
ankush-notifi <ankush.kumar@notifi.network>
2024-05-12 21:44:02 +0530

    test commit

--- packages/notifi-wallet-provider/lib/utils/localStorageUtils.ts
+++ packages/notifi-wallet-provider/lib/utils/localStorageUtils.ts
@@ -6,6 +6,7 @@ import {
 } from '../types';
 
 const localStorageKey = 'NotifiWalletStorage';
+console.log('adding test log===>>');
 
 export const setWalletKeysToLocalStorage = <T extends keyof Wallets>(
   wallet: T,
