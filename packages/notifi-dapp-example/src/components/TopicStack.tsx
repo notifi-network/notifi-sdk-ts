3103dcd959c5f053567cb0a3523d9b93d7081d36
Hao Wang <ge_nah@hotmail.com>
2024-05-13 10:58:07 -0700

    add stack topic row to support price pair alert (#562)
    * add stack topic row
    
    * update alert subscription components name

---
+++ packages/notifi-dapp-example/src/components/TopicStack.tsx
@@ -0,0 +1,79 @@
+import { Icon } from '@/assets/Icon';
+import { TopicStackAlert } from '@/utils/topic';
+import { UserInputOptions } from '@notifi-network/notifi-frontend-client';
+import { useNotifiTopicContext } from '@notifi-network/notifi-react';
+import React from 'react';
+
+type TopicStackProps = {
+  topicStackAlert: TopicStackAlert;
+};
+
+export const TopicStack: React.FC<TopicStackProps> = (props) => {
+  const { unsubscribeAlert, getAlertFilterOptions } = useNotifiTopicContext();
+  const userInputOptions = React.useMemo(() => {
+    const input = getAlertFilterOptions(props.topicStackAlert.alertName)?.input;
+    if (!input) return;
+    return Object.values(input)[0];
+  }, [getAlertFilterOptions]);
+  const userInputOptionValuesToBeDisplayed =
+    userInputOptions &&
+    isAboveOrBelowThresholdUserInputOptions(userInputOptions)
+      ? sortAboveOrBelowThresholdUserInputOptionValue(userInputOptions)
+      : [];
+  return (
+    <div className="flex flex-row justify-between items-center py-3">
+      <div className="flex flex-col items-start">
+        <div className="text-sm font-regular text-notifi-text">
+          {props.topicStackAlert.subscriptionValueInfo.label}
+        </div>
+        <div className="flex flex-row text-xs font-regular text-notifi-text-light">
+          {userInputOptionValuesToBeDisplayed.map((option, id) => (
+            <div key={id} className="mr-1 mt-1">
+              {option}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div
+        className=""
+        onClick={() => unsubscribeAlert(props.topicStackAlert.alertName)}
+      >
+        <Icon
+          id="trash-btn"
+          className="text-notifi-text-light top-6 left-4 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
+
+// Utils
+
+// TODO: refactor & consolidate this type with `UserInputOptions` from `@notifi-network/notifi-frontend-client`
+type AboveOrBelowThresholdUserInputOptions = Record<
+  'threshold' | 'thresholdDirection',
+  string
+>;
+
+const isAboveOrBelowThresholdUserInputOptions = (
+  userInputOptions: UserInputOptions,
+): userInputOptions is AboveOrBelowThresholdUserInputOptions => {
+  return !!userInputOptions.threshold && !!userInputOptions.thresholdDirection;
+};
+
+const sortAboveOrBelowThresholdUserInputOptionValue = (
+  filterOptions: Record<'threshold' | 'thresholdDirection', string>,
+) => {
+  const sortedFilterOptions = [];
+  if (filterOptions.thresholdDirection) {
+    const thresholdDirection =
+      filterOptions.thresholdDirection[0].toUpperCase() +
+      filterOptions.thresholdDirection.slice(1);
+    sortedFilterOptions.push(thresholdDirection);
+  }
+  if (filterOptions.threshold) {
+    sortedFilterOptions.push(filterOptions.threshold);
+  }
+  return sortedFilterOptions;
+};
