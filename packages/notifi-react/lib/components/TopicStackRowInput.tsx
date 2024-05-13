1ecab3b6f8aa93fe45944f34423de76e7985fda1
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-05-13 17:04:50 +0900

    notifi-react/mvp-4553: TopicStackRow (#560)
    * MVP-4569: TopicStack
    
    * MVP-4568: TopicRowStackInput and related components
    
    * MVP-4553: TopicStackRow
    
    * MVP-4553-patch: Finalize TopicStack & refactor

---
+++ packages/notifi-react/lib/components/TopicStackRowInput.tsx
@@ -0,0 +1,124 @@
+import { FusionEventTopic } from '@notifi-network/notifi-frontend-client';
+import clsx from 'clsx';
+import React from 'react';
+
+import { useNotifiTopicContext } from '../context';
+import { useTopicStackRowInput } from '../hooks/useTopicStackRowInput';
+import {
+  defaultCopy,
+  getFusionEventMetadata,
+  getUpdatedAlertFilterOptions,
+  getUserInputParams,
+  isAlertFilter,
+} from '../utils';
+import { ErrorGlobal } from './ErrorGlobal';
+import { LoadingAnimation } from './LoadingAnimation';
+import { SubscriptionValueInput } from './SubscriptionValueInput';
+import { TopicOptions } from './TopicOptions';
+
+export type TopicStackRowInputProps = {
+  classNames?: {
+    container?: string;
+    button?: string;
+  };
+  topic: FusionEventTopic;
+  onSave?: () => void;
+  copy?: {
+    buttonContent?: string;
+  };
+};
+
+export const TopicStackRowInput: React.FC<TopicStackRowInputProps> = (
+  props,
+) => {
+  const subscriptionValueOrRef = getFusionEventMetadata(props.topic)
+    ?.uiConfigOverride?.subscriptionValueOrRef;
+
+  if (!subscriptionValueOrRef) {
+    return null; // TODO: handle undefined or error
+  }
+  const { isLoading: isLoadingTopic, error: topicError } =
+    useNotifiTopicContext();
+
+  const filterName = getFusionEventMetadata(props.topic)?.filters.find(
+    isAlertFilter,
+  )?.name;
+
+  const userInputParams = getUserInputParams(props.topic);
+
+  const {
+    subscriptionValue,
+    setSubscriptionValue,
+    filterOptionsToBeSubscribed,
+    setFilterOptionsToBeSubscribed,
+    subscribeTopic,
+    isTopicReadyToSubscribe,
+  } = useTopicStackRowInput(
+    props.topic,
+    userInputParams,
+    filterName,
+    props.onSave,
+  );
+
+  if (topicError) {
+    return <ErrorGlobal />; // TODO: handle error
+  }
+
+  return (
+    <div
+      className={clsx(
+        'notifi-topic-stack-row-input',
+        props.classNames?.container,
+      )}
+    >
+      <SubscriptionValueInput
+        subscriptionValueRef={subscriptionValueOrRef}
+        subscriptionValue={subscriptionValue}
+        setSubscriptionValue={setSubscriptionValue}
+      />
+      {subscriptionValue && userInputParams.length > 0 ? (
+        <div className={clsx('notifi-topic-row-user-inputs-row-container')}>
+          {userInputParams.map((userInputParm, id) => {
+            return (
+              <TopicOptions<'standalone'>
+                key={id}
+                userInputParam={userInputParm}
+                topic={props.topic}
+                onSelectAction={{
+                  actionType: 'updateFilterOptions',
+                  action: (userInputParmName, option) => {
+                    if (!filterOptionsToBeSubscribed || !filterName) return;
+                    const updatedAlertFilterOptiopns =
+                      getUpdatedAlertFilterOptions(
+                        filterName,
+                        filterOptionsToBeSubscribed,
+                        userInputParmName,
+                        option,
+                      );
+                    setFilterOptionsToBeSubscribed(updatedAlertFilterOptiopns);
+                  },
+                }}
+              />
+            );
+          })}
+          <div
+            className={clsx(
+              'notifi-topic-stack-row-input-button',
+              props.classNames?.button,
+              !isTopicReadyToSubscribe && 'disabled',
+            )}
+          >
+            {isLoadingTopic ? (
+              <LoadingAnimation type={'spinner'} />
+            ) : (
+              <div onClick={subscribeTopic}>
+                {props.copy?.buttonContent ??
+                  defaultCopy.topicStackRowInput.buttonContent}
+              </div>
+            )}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
