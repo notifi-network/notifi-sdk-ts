285525fc4867285e036fbd11ad78347d906a8d28
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-05-13 17:04:50 +0900

    notifi-react/mvp-4553: TopicStackRow (#560)
    * MVP-4569: TopicStack
    
    * MVP-4568: TopicRowStackInput and related components
    
    * MVP-4553: TopicStackRow
    
    * MVP-4553-patch: Finalize TopicStack & refactor

---
+++ packages/notifi-react/lib/hooks/useTopicStackRowInput.tsx
@@ -0,0 +1,99 @@
+import {
+  FusionEventTopic,
+  FusionFilterOptions,
+  InputObject,
+  UiType,
+  UserInputParam,
+} from '@notifi-network/notifi-frontend-client';
+import React from 'react';
+
+import { useNotifiTargetContext, useNotifiTopicContext } from '../context';
+import { composeTopicStackAlertName } from '../utils';
+
+export const useTopicStackRowInput = (
+  topic: FusionEventTopic,
+  userInputParams: UserInputParam<UiType>[],
+  filterName?: string,
+  onSave?: () => void,
+) => {
+  const [subscriptionValue, setSubscriptionValue] =
+    React.useState<InputObject | null>(null);
+  const [filterOptionsToBeSubscribed, setFilterOptionsToBeSubscribed] =
+    React.useState<FusionFilterOptions | null>(null);
+
+  React.useEffect(() => {
+    // Initial set up for filterOptionsToBeSubscribed
+    if (userInputParams && filterName) {
+      const input: FusionFilterOptions['input'] = {
+        [filterName]: {},
+      };
+      userInputParams.forEach((userInputParam) => {
+        input[filterName][userInputParam.name] = '';
+      });
+      setFilterOptionsToBeSubscribed({
+        version: 1,
+        input,
+      });
+    }
+  }, []);
+
+  const isUserInputParamsValid = React.useMemo(() => {
+    if (filterOptionsToBeSubscribed && filterName) {
+      filterOptionsToBeSubscribed.input[filterName];
+      const isAllFieldsInputted = userInputParams.every((userInputParam) => {
+        const input = filterOptionsToBeSubscribed.input;
+        return (
+          !!input[filterName][userInputParam.name] &&
+          input[filterName][userInputParam.name] !== ''
+        );
+      });
+      return isAllFieldsInputted ? true : false;
+    }
+    return false;
+  }, [filterOptionsToBeSubscribed]);
+
+  const { subscribeAlertsWithFilterOptions } = useNotifiTopicContext();
+  const {
+    targetDocument: { targetGroupId },
+  } = useNotifiTargetContext();
+
+  const isTopicReadyToSubscribe = !!(
+    filterOptionsToBeSubscribed &&
+    subscriptionValue &&
+    targetGroupId &&
+    isUserInputParamsValid
+  );
+
+  const subscribeTopic = async () => {
+    if (!isTopicReadyToSubscribe) return;
+    const alertName = composeTopicStackAlertName(
+      topic.uiConfig.name,
+      subscriptionValue.value,
+      subscriptionValue.label,
+    );
+
+    await subscribeAlertsWithFilterOptions(
+      [
+        {
+          topic: topic,
+          filterOptions: filterOptionsToBeSubscribed,
+          customAlertName: alertName,
+          subscriptionValue: subscriptionValue.value,
+        },
+      ],
+      targetGroupId,
+    );
+    setSubscriptionValue(null);
+    onSave && onSave();
+  };
+
+  return {
+    subscriptionValue,
+    setSubscriptionValue,
+    filterOptionsToBeSubscribed,
+    setFilterOptionsToBeSubscribed,
+    isUserInputParamsValid,
+    subscribeTopic,
+    isTopicReadyToSubscribe,
+  };
+};
