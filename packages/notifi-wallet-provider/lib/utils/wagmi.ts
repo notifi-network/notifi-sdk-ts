02e5f21d6e7197f7d76c6e51de90b4bd771a97bc
ankush-notifi <151224544+ankush-notifi@users.noreply.github.com>
2024-05-14 14:34:36 +0530

    Ankush/mvp 4508 new (#565)
    * update css style for GMX (#532)
    
    * update css style for GMX
    
    * Ankush/mvp 4347 (#549)
    
    * update css style for GMX
    
    * remove unused wallets
    
    * fix type error in NotifiFrontendClientContext
    
    * fix eslint error
    
    * added coinbase wallet support
    
    * metamask  wallet support
    
    * wallet support for - rabby, coinbase, rainbow, zerion, okx
    
    * removed coinbase custom hook
    
    * fixed autoconnect function
    
    * wallet support for - binance
    
    * useBinance hook added
    
    * wallet-connect support
    
    * textual change
    
    * coinbase QR code support
    
    * wallet integration on GMX
    
    * revert metamask hook changes
    
    * GMX supported wallets added
    
    ---------
    
    Co-authored-by: Hao Wang <ge_nah@hotmail.com>
    
    * use context from notifi-react package and update alert subscription section for new event type
    
    * fix alert subscription layout
    
    * create SubscriptionValueInput component
    
    * fix type error
    
    * comment code for slack destination
    
    * remove subscription block
    
    * web3 wallet FE integration
    
    * web3 wallet BE integration
    
    * fix subscribe to health check event
    
    * fix renew target group for discord, slack and wallet alert
    
    * toggle target fix
    
    * target context and wallet support refactor
    
    * wallet support refactor
    
    * added CBW subscribe logic
    
    * added CBW subscribe message
    
    * Add xmtp verification mutations to notifi-graphql
    
    * Add xmtp signature and xmtp verification
    
    * loader removed
    
    * website-url added for all wallets
    
    * css added for not installed wallets
    
    * update css style for GMX (#532)
    
    * update css style for GMX
    
    * add GMX wallets
    
    * use context from notifi-react package and update alert subscription section for new event type
    
    * fix alert subscription layout
    
    * create SubscriptionValueInput component
    
    * fix type error
    
    * comment code for slack destination
    
    * remove subscription block
    
    * fix subscribe to health check event
    
    * add GMX wallets part-2
    
    * fix type error
    
    * Add xip42 signature implementation
    
    * tooltip added for wallet alert
    
    * import context in dapp example + create topic components (#550)
    
    * use context from notifi-react package and update alert subscription section for new event type
    
    * fix alert subscription layout
    
    * create SubscriptionValueInput component
    
    * fix type error
    
    * comment code for slack destination
    
    * remove subscription block
    
    * fix subscribe to health check event
    
    * add GMX wallets part-2
    
    * fix type error
    
    * AlertSubscription/AlertSubscriptionRow/AlertSubscriptionOptions updates (#557)
    
    * update AlertSubscription/AlertSubscriptionRow/AlertSubscriptionOptions to support group topic
    
    * update subscriptionValueOrRef type
    
    * add comment
    
    * simplify alert subscription type name
    
    * Fix cbw subscribe endpoint
    
    * Finish xip 42 impl
    
    * wallet-provider-sdk fix
    
    * eslint fix
    
    * test commit
    
    * gmx feedback
    
    * code refactoring
    
    * removed XMTP file
    
    * added XMTP file
    
    * EVM address
    
    ---------
    
    Co-authored-by: Hao Wang <ge_nah@hotmail.com>
    Co-authored-by: Kaito Trias <kaito.trias@notifi.network>
    Co-authored-by: eric-notifi <eric.lee@notifi.network>

--- packages/notifi-wallet-provider/lib/utils/wagmi.ts
+++ packages/notifi-wallet-provider/lib/utils/wagmi.ts
@@ -1,21 +1,36 @@
-import { createConfig, http } from 'wagmi';
+import { configureChains, createConfig } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
-import { coinbaseWallet, walletConnect } from 'wagmi/connectors';
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { publicProvider } from 'wagmi/providers/public';
 
-export const config = createConfig({
-  chains: [mainnet],
-  multiInjectedProviderDiscovery: false,
-  connectors: [
-    coinbaseWallet({ appName: 'Notifi' }), //TODO: make it dynamic
-    walletConnect({
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()],
+);
+
+export const connectors = [
+  new CoinbaseWalletConnector({
+    chains,
+    options: {
+      appName: 'Notifi x GMX',
+    },
+  }),
+  new WalletConnectConnector({
+    chains,
+    options: {
       projectId: '632a105feb9cf8380428a4f240eb6f13',
       qrModalOptions: {
         explorerExcludedWalletIds: 'ALL',
         enableExplorer: false,
       },
-    }),
-  ],
-  transports: {
-    [mainnet.id]: http(),
-  },
+    },
+  }),
+];
+
+export const config = createConfig({
+  autoConnect: false,
+  connectors: connectors,
+  publicClient,
+  webSocketPublicClient,
 });
