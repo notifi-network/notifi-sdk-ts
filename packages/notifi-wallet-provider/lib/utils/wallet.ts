28bfa7a9c7bea8410d9d85e4aef06d938aa1e084
kaan-notifi <kaan.kolcu@notifi.network>
2024-05-18 03:25:19 +0400

    leap and phantom wallet install links added

--- packages/notifi-wallet-provider/lib/utils/wallet.ts
+++ packages/notifi-wallet-provider/lib/utils/wallet.ts
@@ -3,6 +3,8 @@ import { Wallets } from '../types';
 export const walletsWebsiteLink: { [key in keyof Wallets]: string } = {
   metamask: 'https://metamask.io/download/',
   keplr: 'https://www.keplr.app/download',
+  leap: 'https://www.leapwallet.io/download',
+  phantom: 'https://phantom.app/download',
   binance: 'https://www.bnbchain.org/en/binance-wallet',
   okx: 'https://www.okx.com/web3',
   rabby: 'https://rabby.io/',
