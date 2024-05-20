63fd3864ac8925d55fca167083633276610b6c42
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-05-20 16:14:35 +0900

    notifi-react/MVP-4636+4683: Finalize InboxHistory & Impl infinite scroll for HistoryList (#581)
    * MVP-4636-1: Add logic for InboxHistoryDetail
    
    * MVP-4636-2: Finalize InboxHistory & Refactor
    
    * MVP-4683: Light weight implementation of infinite scroll for HistoryList

---
+++ packages/notifi-react/lib/components/HistoryList.tsx
@@ -0,0 +1,142 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import { Icon, IconType } from '../assets/Icons';
+import { HistoryItem, useNotifiHistoryContext } from '../context';
+import { defaultCopy, defaultLoadingAnimationStyle } from '../utils';
+import { HistoryRow } from './HistoryRow';
+import { LoadingAnimation } from './LoadingAnimation';
+import { NavHeader } from './NavHeader';
+
+type HistoryListProps = {
+  classNames?: {
+    container?: string;
+    main?: string;
+    button?: string;
+    loadingSpinner?: React.CSSProperties;
+    loadingMore?: string;
+    inboxEmpty?: string;
+    emptyIcon?: string;
+    emptyTitle?: string;
+    emptyDescription?: string;
+  };
+  copy?: {
+    headerTitle?: string;
+    buttonText?: string;
+    emptyTitle?: string;
+    emptyDescription?: string;
+  };
+  emptyIcon?: IconType;
+  setSelectedHistoryItem: React.Dispatch<
+    React.SetStateAction<HistoryItem | null>
+  >;
+  isHidden: boolean;
+};
+
+export const HistoryList: React.FC<HistoryListProps> = (props) => {
+  const {
+    historyItems,
+    hasNextPage,
+    getHistoryItems,
+    isLoading: isLoadingHistoryItems,
+  } = useNotifiHistoryContext();
+  const [isLoadingMoreItems, setIsLoadingMoreItems] = React.useState(false);
+  const loadingSpinnerStyle: React.CSSProperties =
+    props.classNames?.loadingSpinner ?? defaultLoadingAnimationStyle.spinner;
+  const mainRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    // NOTE: Light weight implementation of infinite scroll
+    const scrollDetectedAction = async () => {
+      const scrollTop = mainRef.current?.scrollTop ?? 0;
+      const scrollHeight = mainRef.current?.scrollHeight ?? 0;
+      const offsetHeight = mainRef.current?.offsetHeight ?? 0;
+      const edgeBuffer = 10; // this buffer vary based on the css config (border, padding, margin size)
+      if (scrollTop > scrollHeight - offsetHeight - edgeBuffer) {
+        if (isLoadingHistoryItems || !hasNextPage) return;
+        setIsLoadingMoreItems(true);
+        await getHistoryItems();
+        setIsLoadingMoreItems(false);
+      }
+    };
+    mainRef.current?.addEventListener('scroll', scrollDetectedAction);
+    return () =>
+      mainRef.current?.removeEventListener('scroll', scrollDetectedAction);
+  }, [hasNextPage, isLoadingHistoryItems]);
+
+  return (
+    <div
+      className={clsx(
+        'notifi-history-list',
+        props.isHidden && 'hidden',
+        props.classNames?.container,
+      )}
+    >
+      <NavHeader>
+        {props.copy?.headerTitle ?? defaultCopy.inboxHistoryList.headerTitle}
+      </NavHeader>
+      <div
+        ref={mainRef}
+        className={clsx('notifi-history-list-main', props.classNames?.main)}
+      >
+        {/* HistoryList */}
+        {historyItems.map((item, id) => (
+          <HistoryRow
+            key={id}
+            historyItem={item}
+            onClick={() => props.setSelectedHistoryItem(item)}
+          />
+        ))}
+        {/* Load more */}
+        {isLoadingMoreItems ? (
+          <div
+            className={clsx(
+              'notifi-history-list-loading-more',
+              props.classNames?.loadingMore,
+              !hasNextPage && 'hidden',
+            )}
+          >
+            <LoadingAnimation type="spinner" {...loadingSpinnerStyle} />
+          </div>
+        ) : null}
+        {/* EmptyInbox */}
+        {!isLoadingHistoryItems && historyItems.length === 0 ? (
+          <div
+            className={clsx(
+              'notifi-history-list-empty',
+              props.classNames?.inboxEmpty,
+            )}
+          >
+            <div
+              className={clsx(
+                'notifi-history-list-empty-icon',
+                props.classNames?.emptyIcon,
+              )}
+            >
+              <Icon type={props.emptyIcon ?? 'empty-box'} />
+            </div>
+
+            <div
+              className={clsx(
+                'notifi-history-list-empty-title',
+                props.classNames?.emptyTitle,
+              )}
+            >
+              {props.copy?.emptyTitle ??
+                defaultCopy.historyList.inboxEmptyTitle}
+            </div>
+            <div
+              className={clsx(
+                'notifi-history-list-empty-description',
+                props.classNames?.emptyDescription,
+              )}
+            >
+              {props.copy?.emptyDescription ??
+                defaultCopy.historyList.inboxEmptyDescription}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
