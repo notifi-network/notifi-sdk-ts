6a79ec2a5a7266d983344a173219c2e989b11191
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-05-24 22:43:19 +0900

    MVP-4639: InboxConfigTargetList (#591)

--- packages/notifi-react/lib/components/InboxConfigTargetList.tsx
+++ packages/notifi-react/lib/components/InboxConfigTargetList.tsx
@@ -1,15 +1,41 @@
 import clsx from 'clsx';
 import React from 'react';
 
+import { useNotifiTargetContext } from '../context';
+import { defaultCopy, defaultLoadingAnimationStyle, hasTarget } from '../utils';
+import { InboxView } from './Inbox';
+import { LoadingAnimation } from './LoadingAnimation';
+import { NavHeader } from './NavHeader';
+import { TargetList } from './TargetList';
+
 export type InboxConfigTargetListProps = {
   classNames?: {
     container?: string;
+    main?: string;
+    button?: string;
+    loadingSpinner?: React.CSSProperties;
   };
+  copy?: {
+    header?: string;
+    buttonText?: string;
+  };
+  setInboxView: React.Dispatch<React.SetStateAction<InboxView>>;
 };
 
 export const InboxConfigTargetList: React.FC<InboxConfigTargetListProps> = (
   props,
 ) => {
+  const loadingSpinnerStyle: React.CSSProperties =
+    props.classNames?.loadingSpinner ?? defaultLoadingAnimationStyle.spinner;
+  const {
+    isLoading,
+    targetDocument: { targetData },
+  } = useNotifiTargetContext();
+
+  if (!hasTarget(targetData)) {
+    props.setInboxView(InboxView.InboxConfigTopic);
+  }
+
   return (
     <div
       className={clsx(
@@ -17,7 +43,52 @@ export const InboxConfigTargetList: React.FC<InboxConfigTargetListProps> = (
         props.classNames?.container,
       )}
     >
-      Dummy Inbox Config Target List
+      <NavHeader
+        leftCta={{
+          icon: 'arrow-back',
+          action: () => props.setInboxView(InboxView.InboxConfigTopic),
+        }}
+      >
+        {props.copy?.header ?? defaultCopy.inboxConfigTargetList.header}
+      </NavHeader>
+      <div
+        className={clsx(
+          'notifi-inbox-config-target-list-main',
+          props.classNames?.main,
+        )}
+      >
+        <TargetList
+          classNames={{
+            targetListVerifiedItem: 'notifi-inbox-config-target-list-item',
+            TargetCta: {
+              actionNotRequired:
+                'notifi-inbox-config-target-list-item-verified',
+            },
+          }}
+          parentComponent="inbox"
+        />
+      </div>
+      <button
+        className={clsx(
+          'notifi-inbox-config-target-list-button',
+          props.classNames?.button,
+        )}
+        disabled={isLoading}
+        onClick={() => props.setInboxView(InboxView.InboxConfigTargetEdit)}
+      >
+        {isLoading ? (
+          <LoadingAnimation type="spinner" {...loadingSpinnerStyle} />
+        ) : null}
+        <div
+          className={clsx(
+            'notifi-inbox-config-target-list-button-text',
+            isLoading && 'hidden',
+          )}
+        >
+          {props.copy?.buttonText ??
+            defaultCopy.inboxConfigTargetList.buttonText}
+        </div>
+      </button>
     </div>
   );
 };
