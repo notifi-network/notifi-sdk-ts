fedaa1a383c41d2f7142a9cf25794ca3eafcbe82
Kaito Trias <kaito.trias@notifi.network>
2024-05-30 18:14:58 -0600

    Add gmx build configuration

--- packages/notifi-dapp-example/next.config.mjs
+++ packages/notifi-dapp-example/next.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('next').NextConfig} */
+import CopyPlugin from "copy-webpack-plugin";
+
 const nextConfig = {
-  // output: 'export' /** BUILD ONLY */,
+  output: 'export' /** BUILD ONLY */,
   images: {
     formats: ['image/webp'],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.plugins.push(new CopyPlugin({
+      patterns: [
+        { from: "public/wasm", to: "./server/chunks" },
+      ],
+    }))
+    return config;
   },
 };
 
