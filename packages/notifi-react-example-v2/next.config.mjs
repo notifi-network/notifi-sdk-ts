5d66546d4984ef9787427d1769160379da5a2b2c
eric-notifi <eric.lee@notifi.network>
2024-06-11 19:11:29 +0900

    chore: adjust notifi-example-v2 config file for wasm (rebase main error after merging integration-GMX

--- packages/notifi-react-example-v2/next.config.mjs
+++ packages/notifi-react-example-v2/next.config.mjs
@@ -1,6 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export' /** BUILD ONLY */,
+import { log } from 'console';
+import CopyPlugin from 'copy-webpack-plugin';
+import process from 'process';
+
+const args = process.argv;
+
+log(args.join(' '));
+
+let nextConfig = {
+  webpack: (config) => {
+    /** ⬇️ For WalletConnect SSR: https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration */
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    /** ⬇️ For @xmpt/sdk requires .wasm bundled  */
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: 'public/wasm', to: './server/vendor-chunks' }],
+      }),
+    );
+    return config;
+  },
 };
+
+if (args.includes('build')) {
+  nextConfig = {
+    ...nextConfig,
+    /** ⬇️ For including static index.html: https://nextjs.org/docs/app/building-your-application/deploying/static-exports */
+    output: 'export',
+    /** ⬇️ For @xmpt/sdk requires .wasm bundled  */
+    webpack: (config) => {
+      /** ⬇️ For WalletConnect SSR: https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration */
+      config.externals.push('pino-pretty', 'lokijs', 'encoding');
+      /** ⬇️ For @xmpt/sdk requires .wasm bundled  */
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [{ from: 'public/wasm', to: './server/chunks' }],
+        }),
+      );
+      return config;
+    },
+  };
+}
 
 export default nextConfig;
