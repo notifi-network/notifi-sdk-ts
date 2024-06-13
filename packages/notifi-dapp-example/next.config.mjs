612386c3fca56bc9577af879661e112e119b722e
Kaito Trias <kaito.trias@notifi.network>
2024-06-13 15:22:06 -0600

    Update build config

--- packages/notifi-dapp-example/next.config.mjs
+++ packages/notifi-dapp-example/next.config.mjs
@@ -2,33 +2,33 @@
 import CopyPlugin from "copy-webpack-plugin";
 
 const nextConfig = {
-  output: 'export' /** BUILD ONLY */,
+  // output: 'export' /** BUILD ONLY */,
   images: {
     formats: ['image/webp'],
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.plugins.push(new CopyPlugin({
-      patterns: [
-        { from: "public/wasm", to: "./server/chunks" },
-      ],
-    }))
-    return config;
-  },
+  // typescript: { /** BUILD ONLY */
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: { /** BUILD ONLY */
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
+  // webpack: ( /** BUILD ONLY */
+  //   config,
+  //   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  // ) => {
+  //   config.plugins.push(new CopyPlugin({
+  //     patterns: [
+  //       { from: "public/wasm", to: "./server/chunks" },
+  //     ],
+  //   }))
+  //   return config;
+  // },
 };
 
 export default nextConfig;
