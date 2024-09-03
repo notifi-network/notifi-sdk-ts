0a1b188328f1321e9ff719234f649b72cc598a31
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-09-03 13:08:35 +0900

    MVP-5365+3566+568: frontendClient / notifi-react  update for loginByOidc (#669)
    * MVP-5365+3566+568: FrontendClient / notifi-react  update for loginByOauth
    
    * MVP-5394-2: migrate loginByOauth to loginByOidc

--- packages/notifi-frontend-client/lib/storage/LocalForageStorageDriver.ts
+++ packages/notifi-frontend-client/lib/storage/LocalForageStorageDriver.ts
@@ -4,6 +4,7 @@ import {
   NotifiEnvironment,
   NotifiFrontendConfiguration,
   checkIsConfigWithDelegate,
+  checkIsConfigWithOidc,
   checkIsConfigWithPublicKeyAndAddress,
 } from '../configuration/NotifiFrontendConfiguration';
 import { StorageDriver } from './NotifiFrontendStorage';
@@ -32,8 +33,9 @@ export const createLocalForageStorageDriver = (
   let keyPrefix = `${getEnvPrefix(config.env)}:${config.tenantId}:${
     config.walletBlockchain
   }`;
-
-  if (checkIsConfigWithPublicKeyAndAddress(config)) {
+  if (checkIsConfigWithOidc(config)) {
+    keyPrefix += `:${config.userAccount}`;
+  } else if (checkIsConfigWithPublicKeyAndAddress(config)) {
     keyPrefix += `:${config.accountAddress}:${config.authenticationKey}`;
   } else if (checkIsConfigWithDelegate(config)) {
     keyPrefix += `:${config.delegatorAddress}`;
