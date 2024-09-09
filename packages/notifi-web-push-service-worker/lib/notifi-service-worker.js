afee4f8015b7c7e6863f9fa319513de11ce2c7e7
Kaito Trias <kaito.trias@notifi.network>
2024-09-09 12:26:40 -0600

    Remove skip waiting in service worker

--- packages/notifi-web-push-service-worker/lib/notifi-service-worker.js
+++ packages/notifi-web-push-service-worker/lib/notifi-service-worker.js
@@ -59,10 +59,6 @@ function createDb() {
   };
 }
 
-self.addEventListener('install', function (event) {
-  self.skipWaiting();
-})
-
 function urlBase64ToUint8Array(base64String) {
   var padding = '='.repeat((4 - base64String.length % 4) % 4);
   var base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
