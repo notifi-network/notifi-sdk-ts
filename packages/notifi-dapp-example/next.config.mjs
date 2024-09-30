f351ad788bc05ebabd40dcc88f3f6477cdefd714
eric-notifi <eric.lee@notifi.network>
2024-09-30 19:33:49 +0900

    Update dapp-example next config

--- packages/notifi-dapp-example/next.config.mjs
+++ packages/notifi-dapp-example/next.config.mjs
@@ -1,37 +1,26 @@
 /** @type {import('next').NextConfig} */
-import CopyPlugin from "copy-webpack-plugin";
 import process from 'process';
 
 const args = process.argv;
 
 let nextConfig = {
-  // output: 'export' /** BUILD ONLY */,
   images: {
     formats: ['image/webp'],
   },
-  
-  // typescript: { /** BUILD ONLY */
-  //   // !! WARN !!
-  //   // Dangerously allow production builds to successfully complete even if
-  //   // your project has type errors.
-  //   // !! WARN !!
-  //   ignoreBuildErrors: true,
-  // },
-  // eslint: { /** BUILD ONLY */
-  //   // Warning: This allows production builds to successfully complete even if
-  //   // your project has ESLint errors.
-  //   ignoreDuringBuilds: true,
-  // },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.plugins.push(new CopyPlugin({
-      patterns: [
-        // { from: "public/wasm", to: "./server/chunks" }, /** BUILD ONLY */
-        { from: "public/wasm", to: "./server/vendor-chunks/" },
-      ],
-    }))
+  /** ⬇For @xmpt/sdk (external bindings wasm files) - Nextjs version >= '^14.2.0'   */
+  experimental: {
+    serverComponentsExternalPackages: ['@xmtp/user-preferences-bindings-wasm'],
+  },
+
+  webpack: (config) => {
+    /** ⬇️ For WalletConnect SSR: https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration */
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    /** ⬇For @xmpt/sdk (external bindings wasm files) - Nextjs version < '^14.2.0' */
+    //   config.plugins.push(
+    //     new CopyPlugin({
+    //       patterns: [{ from: 'public/wasm', to: './server/vendor-chunks/' }],
+    //     }),
+    //   );
     return config;
   },
 };
@@ -39,32 +28,36 @@ let nextConfig = {
 if (args.includes('build')) {
   nextConfig = {
     ...nextConfig,
-  output: 'export' /** BUILD ONLY */,
-  
-  typescript: { /** BUILD ONLY */
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: { /** BUILD ONLY */
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.plugins.push(new CopyPlugin({
-      patterns: [
-        { from: "public/wasm", to: "./server/chunks" }, /** BUILD ONLY */
-        { from: "public/wasm", to: "./server/vendor-chunks/" },
-      ],
-    }))
-    return config;
-  },
+    /** ⬇ For including static index.html: https://nextjs.org/docs/app/building-your-application/deploying/static-exports */
+    output: 'export',
+
+    // TODO: Remove below two blocks (eslint and typescript): rather than ignoring errors during build, fix them in the codebase
+    typescript: {
+      /** BUILD ONLY */
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      /** BUILD ONLY */
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+
+    webpack: (config) => {
+      /** ⬇️ For WalletConnect SSR: https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration */
+      config.externals.push('pino-pretty', 'lokijs', 'encoding');
+      /** ⬇For @xmpt/sdk (external bindings wasm files) - Nextjs version < '^14.2.0' */
+      // config.plugins.push(
+      //   new CopyPlugin({
+      //     patterns: [{ from: 'public/wasm', to: './server/chunks' }],
+      //   }),
+      // );
+      return config;
+    },
   };
 }
 
