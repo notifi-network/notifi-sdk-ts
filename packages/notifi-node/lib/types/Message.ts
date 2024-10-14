f538f42d6a475b0032e3cda0c277593098c3cab6
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-10-15 08:48:58 +0900

    MVP-5584: notifi-node refresh - Deprecate methods & Refactor existing (#703)
    * MVP-5584-1: restructure types & deprecate legacy methods
    
    * MVP-5584-2: refactor index.ts / utils/ package.json & rename NotifiClient to NotifiNodeClient

--- packages/notifi-node/lib/types/Message.ts
+++
@@ -1,6 +0,0 @@
-import type { Types as Gql } from '@notifi-network/notifi-graphql';
-
-export type Message<Type extends Gql.MessageType, Payload> = Readonly<{
-  type: Type;
-  payload: Payload;
-}>;
