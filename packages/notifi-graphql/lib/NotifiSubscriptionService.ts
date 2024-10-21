603adbe7b852913020315eec852d951ad5585307
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-10-21 13:21:03 +0900

    MVP-5600: Service emitter pattern: NotifiEventEmitter instance (#709)
    * MVP-5600-1: impliment NotifiEmitterEvent for notifi-gql service
    
    * MVP-5600-2: Refactor subscription query using fragment (tenantEntityChanged)
    
    * MVP-5600-3: Refactor EventEmitter for listener callback strict argument type
    
    * MVP-5600-4: remove redundant subscription service in notifi-node

--- packages/notifi-graphql/lib/NotifiSubscriptionService.ts
+++ packages/notifi-graphql/lib/NotifiSubscriptionService.ts
@@ -1,18 +1,27 @@
-import { createClient, Client, SubscribePayload, Event } from 'graphql-ws';
+import {
+  createClient,
+  Client as WebSocketClient,
+  SubscribePayload,
+} from 'graphql-ws';
 import { Observable } from 'relay-runtime';
+import { NotifiEventEmitter, NotifiEmitterEvents } from './NotifiEventEmitter';
 
 /**
  * @param webSocketImpl - A custom WebSocket implementation to use instead of the one provided by the global scope. Mostly useful for when using the client outside of the browser environment.
  * @ref https://github.com/enisdenjo/graphql-ws/blob/c030ed1d5f7e8a552dffbfd46712caf7dfe91a54/src/client.ts#L400
  */
 export class NotifiSubscriptionService {
-  private wsClient: Client | undefined;
+  private wsClient: WebSocketClient | undefined;
   private jwt: string | undefined;
+  private eventEmitter = new NotifiEventEmitter<NotifiEmitterEvents>();
   constructor(
     private wsurl: string,
     private webSocketImpl?: unknown,
   ) {}
 
+  /**
+   * @deprecated Should not directly manipulate the websocket client. Instead use the returned subscription object to manage the subscription. ex. subscription.unsubscribe()
+   */
   disposeClient = () => {
     if (this.wsClient) {
       this.jwt = undefined;
@@ -64,7 +73,21 @@ export class NotifiSubscriptionService {
     return subscription;
   };
 
-  private _toObservable(client: Client, operation: SubscribePayload) {
+  addEventListener = <K extends keyof NotifiEmitterEvents>(
+    event: K,
+    callBack: (...args: NotifiEmitterEvents[K]) => void,
+  ) => {
+    return this.eventEmitter.on(event, callBack);
+  };
+
+  removeEventListener = <K extends keyof NotifiEmitterEvents>(
+    event: K,
+    callBack: (...args: NotifiEmitterEvents[K]) => void,
+  ) => {
+    return this.eventEmitter.off(event, callBack);
+  };
+
+  private _toObservable(client: WebSocketClient, operation: SubscribePayload) {
     return Observable.create((observer) =>
       client.subscribe(operation, {
         next: (data) => observer.next(data),
@@ -87,22 +110,29 @@ export class NotifiSubscriptionService {
       /* https://github.com/enisdenjo/graphql-ws/blob/c030ed1d5f7e8a552dffbfd46712caf7dfe91a54/src/client.ts#L400 */
       webSocketImpl: this.webSocketImpl,
     });
-    // NOTE: Use wsClient.on method to listen to WebSocket events
-    // TODO: Use event emitter to listen to WebSocket events and emit corresponding events
+
     this.wsClient.on('connecting', () => {
-      console.log('WebSocket connecting');
+      console.log('WebSocket connecting'); // TODO: Remove before merge
+      this.eventEmitter.emit('wsConnecting');
     });
 
     this.wsClient.on('connected', () => {
-      console.log('WebSocket connected');
+      console.log('WebSocket connected'); // TODO: Remove before merge
+
+      this.eventEmitter.emit('wsConnected', this.wsClient!);
     });
 
     this.wsClient.on('closed', (event) => {
-      console.log(`WebSocket closed: ${JSON.stringify(event)}`);
+      console.log(`WebSocket closed`); // TODO: Remove before merge
+
+      this.eventEmitter.emit('wsClosed', event);
     });
 
     this.wsClient.on('error', (error) => {
-      console.error('WebSocket error:', error);
+      console.error('WebSocket error:', error); // TODO: Remove before merge
+      if (error instanceof Error) {
+        this.eventEmitter.emit('wsError', error);
+      }
     });
   };
 }
