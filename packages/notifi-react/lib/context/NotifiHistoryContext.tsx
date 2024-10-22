710e20a07b3916be390d9be26a2ff3bde1e12ea3
eric-notifi <eric.lee@notifi.network>
2024-10-22 10:50:09 +0900

    MVP-5630: allow unread count to be card specific

--- packages/notifi-react/lib/context/NotifiHistoryContext.tsx
+++ packages/notifi-react/lib/context/NotifiHistoryContext.tsx
@@ -146,7 +146,7 @@ export const NotifiHistoryContextProvider: FC<
 
         setHistoryItems((existing) =>
           initialLoad
-            ? validHistoryItems.map(parseHistoryItem) ?? []
+            ? (validHistoryItems.map(parseHistoryItem) ?? [])
             : [...existing, ...(validHistoryItems.map(parseHistoryItem) ?? [])],
         );
         setCursorInfo(result.pageInfo);
@@ -173,9 +173,11 @@ export const NotifiHistoryContextProvider: FC<
       cardConfig
     ) {
       getHistoryItems(true);
-      frontendClient.getUnreadNotificationHistoryCount().then(({ count }) => {
-        setUnreadCount(count);
-      });
+      frontendClient
+        .getUnreadNotificationHistoryCount(cardConfig.id ?? undefined)
+        .then(({ count }) => {
+          setUnreadCount(count);
+        });
     }
   }, [frontendClientStatus, getHistoryItems, cardConfig]);
 
