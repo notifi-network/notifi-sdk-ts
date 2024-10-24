1b61e87c3bcbd48f018fd1317bbac23c04185896
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-10-24 11:24:25 +0900

    MVP-5589: Refactor NotifiNodeClient & give fusionMessage strong type (#711)
    * MVP-5589-1: Refactor NotifiNodeClient w/o adding new methods
    
    * MVP-5589-2: Refactor & give fusionMessage strong type

--- packages/notifi-dataplane/lib/types/FusionMessage.ts
+++ packages/notifi-dataplane/lib/types/FusionMessage.ts
@@ -1,9 +1,11 @@
 import { Types } from 'notifi-graphql/dist';
 
-export interface FusionMessage {
+// If payload type undefined, fallback to object by default
+export interface FusionMessage<T extends object = object> {
   eventTypeId: string;
-  variablesJson: object;
-  idempotencyKey?: string;
+
+  variablesJson: VariablesJsonPayload<T>;
+
   specificWallets?: ReadonlyArray<
     Readonly<{
       walletPublicKey: string;
@@ -11,3 +13,28 @@ export interface FusionMessage {
     }>
   >;
 }
+
+export type VariablesJsonPayload<T extends object> =
+  T extends CommunityManagerJsonPayload ? CommunityManagerJsonPayload : object;
+
+export type CommunityManagerJsonPayload = {
+  campaignId: string;
+  Platform: TargetVariables;
+} & {
+  [key in OptionalTargetType]?: TargetVariables;
+};
+
+export type OptionalTargetType = 'Sms' | 'Telegram' | 'Discord' | 'Email';
+
+/**
+ * @param message - only plain text is supported.
+ * @param message__markdown - markdown string here will be converted to html.
+ * @param sourceMessage - usually to be html format source.
+ */
+export type TargetVariables = {
+  subject?: string;
+  message?: string;
+  message__markdown?: string;
+  sourceMessage?: string;
+  [key: string]: string | undefined;
+};
