371d58a2c274eaf44f25dee2a65c9c8163e2fe19
eric-notifi <eric.lee@notifi.network>
2024-11-07 14:39:45 +0900

    MVP-5700-1: Setup jest test env

---
+++ packages/notifi-node/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['.'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
