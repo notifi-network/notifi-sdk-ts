01637d6037e697d237631f3a1f8f2a6da197d8d8
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2025-02-07 09:14:50 +0900

    MVP-6029: Tenant config v2 poc (#782)
    * MVP-6029-1: basic v2 tenant metadata type
    
    * MVP-6029-2: Update fetchTenantConfig (backward compatible)

---
+++ packages/notifi-frontend-client/lib/models/TenantConfig.ts
@@ -0,0 +1,44 @@
+import { Types } from '@notifi-network/notifi-graphql';
+
+import { CardConfigItemV1 } from './SubscriptionCardConfig';
+
+export type TenantInfo = {
+  cardConfig: TenantConfigMetadata;
+  fusionEventTopics: Array<TopicMetadata>;
+};
+
+export type TopicMetadata = {
+  uiConfig: TopicUiConfig;
+  fusionEventDescriptor: Types.FusionEventDescriptor;
+};
+
+/** v2 of CardConfigItemV1 (rename) */
+export type TenantConfigMetadata =
+  | {
+      version: 'v2'; // TODO: TBD
+      id: string;
+      contactInfo: ContactInfoConfig;
+      isContactInfoRequired?: boolean;
+      eventTypes: Array<TopicUiConfig>;
+    }
+  | CardConfigItemV1; // Legacy infra
+
+export type ContactInfoConfig = Record<Target, ContactInfo>;
+
+export type Target = // TODO: Remove type form notifi-react context. Use this instead
+  'email' | 'sms' | 'telegram' | 'discord' | 'slack' | 'wallet';
+
+export type ContactInfo = Readonly<{
+  active: boolean;
+}>;
+
+export type TopicUiConfig = {
+  name: string;
+  type: 'fusion';
+  topicGroupName?: string;
+  index?: number;
+  fusionEventId: string; // TODO: need further refactor (changed from ValueOrRef<string> to string)
+  tooltipContent?: string;
+  optOutAtSignup?: boolean;
+  displayNameOverride?: string;
+};
