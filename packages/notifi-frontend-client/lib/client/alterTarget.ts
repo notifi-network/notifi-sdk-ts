1fa34431e191eba90a6a9f41c60dd906db87d40c
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2025-02-24 10:20:03 +0900

    MVP-6146: New alterTarget method to replace ensureTarget  (#792)
    * MVP-6146-1: Add alterTarget PoC
    
    * MVP-6146-2: Update alterTarget methods

---
+++ packages/notifi-frontend-client/lib/client/alterTarget.ts
@@ -0,0 +1,113 @@
+import { NotifiService } from '@notifi-network/notifi-graphql';
+
+// TODO: remove and reuse
+type Target =
+  | 'email'
+  | 'phoneNumber'
+  | 'telegram'
+  | 'discord'
+  | 'slack'
+  | 'wallet';
+
+// TODO: consolidate
+type CreateTarget = {
+  type: 'create';
+  value: string;
+  target: Target;
+  service: NotifiService;
+};
+
+type DeleteTarget = {
+  type: 'delete';
+  id: string;
+  target: Target;
+  service: NotifiService;
+};
+
+type AlterTarget = CreateTarget | DeleteTarget;
+
+export const alterTarget = async (
+  alterTarget: AlterTarget,
+): Promise<string | undefined> => {
+  const alterTargetService = alterTarget.service;
+  if (alterTarget.type === 'create') {
+    switch (alterTarget.target) {
+      case 'email':
+        const emailTargetMutation = await alterTargetService.createEmailTarget({
+          name: alterTarget.value,
+          value: alterTarget.value,
+        });
+        return emailTargetMutation.createEmailTarget?.id;
+      case 'phoneNumber':
+        const smsTargetMutation = await alterTargetService.createSmsTarget({
+          name: alterTarget.value,
+          value: alterTarget.value,
+        });
+        return smsTargetMutation.createSmsTarget?.id;
+      case 'telegram':
+        const telegramTargetMutation =
+          await alterTargetService.createTelegramTarget({
+            name: alterTarget.value,
+            value: alterTarget.value,
+          });
+        return telegramTargetMutation.createTelegramTarget?.id;
+      case 'discord':
+        const discordTargetMutation =
+          await alterTargetService.createDiscordTarget({
+            name: alterTarget.value,
+            value: alterTarget.value,
+          });
+        return discordTargetMutation.createDiscordTarget?.id;
+      case 'slack':
+        const slackChannelTargetMutation =
+          await alterTargetService.createSlackChannelTarget({
+            name: alterTarget.value,
+            value: alterTarget.value,
+          });
+        return slackChannelTargetMutation.createSlackChannelTarget
+          .slackChannelTarget?.id;
+      case 'wallet':
+        const web3TargetMutation = await alterTargetService.createWeb3Target({
+          name: alterTarget.value,
+          accountId: '',
+          walletBlockchain: 'OFF_CHAIN',
+          web3TargetProtocol: 'XMTP',
+        });
+        return web3TargetMutation.createWeb3Target?.id;
+    }
+  }
+  if (alterTarget.type === 'delete') {
+    switch (alterTarget.target) {
+      case 'email':
+        await alterTargetService.deleteEmailTarget({
+          id: alterTarget.id,
+        });
+        return;
+      case 'phoneNumber':
+        await alterTargetService.deleteSmsTarget({
+          id: alterTarget.id,
+        });
+        return;
+      case 'telegram':
+        await alterTargetService.deleteTelegramTarget({
+          id: alterTarget.id,
+        });
+        return;
+      case 'discord':
+        await alterTargetService.deleteDiscordTarget({
+          id: alterTarget.id,
+        });
+        return;
+      case 'slack':
+        await alterTargetService.deleteSlackChannelTarget({
+          id: alterTarget.id,
+        });
+        return;
+      case 'wallet':
+        await alterTargetService.deleteWeb3Target({
+          id: alterTarget.id,
+        });
+        return;
+    }
+  }
+};
