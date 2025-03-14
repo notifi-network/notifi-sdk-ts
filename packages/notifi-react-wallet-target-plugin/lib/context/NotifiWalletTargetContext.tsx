f3cbf1a769615e0b7ccad78d0ff01f49899249a4
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2025-03-14 09:05:45 +0900

    MVP-6215: New package: notifi-react-wallet-target-plugin (#804)

---
+++ packages/notifi-react-wallet-target-plugin/lib/context/NotifiWalletTargetContext.tsx
@@ -0,0 +1,53 @@
+import {
+  NotifiFrontendClient,
+  WalletWithSignParams,
+} from '@notifi-network/notifi-frontend-client';
+import { Types } from '@notifi-network/notifi-graphql';
+import React, { FC } from 'react';
+
+import { useXmpt } from '../hooks';
+
+export type SignCoinbaseSignature = (
+  frontendClient: NotifiFrontendClient,
+  web3TargetId: Types.Web3Target['id'],
+  senderAddress: string,
+) => Promise<Types.Web3TargetFragmentFragment | undefined>;
+
+export type NotifiWalletTargetContextType = {
+  isLoading: boolean;
+  error: Error | null;
+  signCoinbaseSignature: SignCoinbaseSignature;
+};
+
+const NotifiWalletTargetContext =
+  React.createContext<NotifiWalletTargetContextType>({
+    isLoading: false,
+    error: null,
+    signCoinbaseSignature: async () => {
+      console.error(
+        'NotifiWalletTargetProvider: ERROR - signCoinbaseSignature not implemented',
+      );
+      return undefined;
+    },
+  });
+
+export type NotifiWalletTargetProviderProps = {
+  walletWithSignParams: WalletWithSignParams;
+};
+
+export const NotifiWalletTargetContextProvider: FC<
+  React.PropsWithChildren<NotifiWalletTargetProviderProps>
+> = ({ children, walletWithSignParams }) => {
+  const contextValue: NotifiWalletTargetContextType = useXmpt({
+    walletWithSignParams,
+  });
+
+  return (
+    <NotifiWalletTargetContext.Provider value={contextValue}>
+      {children}
+    </NotifiWalletTargetContext.Provider>
+  );
+};
+
+export const useWalletTargetContext = () =>
+  React.useContext(NotifiWalletTargetContext);
