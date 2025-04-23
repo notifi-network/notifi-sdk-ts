6298a03c7e95503dde116153a1b3a7837684daf8
eric-notifi <eric.lee@notifi.network>
2025-04-23 14:59:49 +0800

    MVP-6355: normalize ActionInput generization

--- packages/notifi-frontend-client/__test__/NotifiSmartLinkClient.test.ts
+++ packages/notifi-frontend-client/__test__/NotifiSmartLinkClient.test.ts
@@ -1,5 +1,12 @@
 import {
+  ActivateSmartLinkActionInput,
+  SmartLinkActionUserInput,
+} from '@notifi-network/notifi-dataplane';
+import {
+  AuthParams,
+  NotifiEnvironment,
   NotifiSmartLinkClient,
+  SmartLinkConfig,
   newSmartLinkClient,
 } from '@notifi-network/notifi-frontend-client';
 import expect from 'expect';
@@ -20,19 +27,16 @@ describe('Notifi SmartLink & Action Unit Tests', () => {
   it('Activate SmartLinkAction', async () => {
     const client = newSmartLinkClient({
       env,
-      authParams: { walletBlockchain: 'ETHEREUM', walletPublicKey: '0x0' },
+      authParams,
     });
-    const response = await client.activateSmartLinkAction({
-      smartLinkId: smartLink,
-      actionId: '1',
-      inputs: {
-        0: {
-          id: '31264646-a318-486f-9877-cbe97e6a9411',
-          type: 'TEXTBOX',
-          value: '2',
-        },
-      },
-    });
+    const { smartLinkConfig } = await client.fetchSmartLinkConfig(smartLink);
+    const activateSmartLinkActionInput = getFirstActionInput(
+      smartLink,
+      smartLinkConfig,
+    );
+    const response = await client.activateSmartLinkAction(
+      activateSmartLinkActionInput,
+    );
     expect(response).toHaveProperty('successMessage');
     expect(response).toHaveProperty('failureMessage');
     expect(response).toHaveProperty('transactions');
@@ -40,5 +44,42 @@ describe('Notifi SmartLink & Action Unit Tests', () => {
 });
 
 // Utils & Constants (TODO: Move to a separate module when growing)
-const env = 'Development'; // TODO: Update to production
+const env: NotifiEnvironment = 'Development'; // TODO: Update to production
 const smartLink = '1302fc05485341eab8931e759cc0a08c'; // TODO: Update to production
+
+const authParams: AuthParams = {
+  walletBlockchain: 'ETHEREUM',
+  walletPublicKey: '0x0',
+};
+
+/* Grab 1st Action and generate a valid user inputs */
+const getFirstActionInput = (
+  smartLinkId: string,
+  smartLinkConfig: SmartLinkConfig,
+): Omit<ActivateSmartLinkActionInput, 'authParams'> => {
+  const smartLinkActions = smartLinkConfig.components.filter(
+    (component) => component.type === 'ACTION',
+  );
+  const actionId = smartLinkActions[0].id;
+  const inputs = smartLinkActions[0].inputs.reduce(
+    (acc: Record<number, SmartLinkActionUserInput>, input, id) => {
+      const userInput: SmartLinkActionUserInput =
+        input.type === 'TEXTBOX'
+          ? {
+              type: 'TEXTBOX',
+              value: '1',
+              id: input.id,
+            }
+          : {
+              type: 'CHECKBOX',
+              value: true,
+              id: input.id,
+            };
+
+      acc[id] = userInput;
+      return acc;
+    },
+    {},
+  );
+  return { smartLinkId, actionId, inputs };
+};
