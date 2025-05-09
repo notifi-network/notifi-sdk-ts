6c23216b800d0d5273d00422ec5e4e9dea5ee247
eric-notifi <eric.lee@notifi.network>
2025-05-09 13:34:19 +0900

    MVP-6356: SmartLink component test PoC

--- packages/notifi-react-example-v2/cypress/component/NotifiSmartLink.cy.tsx
+++ packages/notifi-react-example-v2/cypress/component/NotifiSmartLink.cy.tsx
@@ -21,6 +21,71 @@ describe('NotifiSmartLink Test', () => {
   });
 
   it('Render SmartLink', () => {
-    // TO BE IMPLEMENTED
+    cy.mountSmartLink();
+    cy.wait('@gqlGetSmartLinkConfigQuery');
+    cy.get('.notifi-smartlink-action-btn')
+      .should('exist')
+      .should('be.disabled');
+    cy.get('.notifi-smartlink-action-input-textbox-input')
+      .should('exist')
+      .type('1');
+    cy.get('.notifi-smartlink-action-btn').should('exist').should('be.enabled');
+  });
+
+  it('Execute SmartLink: w/o preAction, ', () => {
+    cy.mountSmartLink();
+    cy.wait('@gqlGetSmartLinkConfigQuery');
+    cy.get('.notifi-smartlink-action-input-textbox-input')
+      .should('exist')
+      .type('1');
+    cy.get('.notifi-smartlink-action-btn')
+      .should('exist')
+      .should('be.enabled')
+      .click();
+    cy.wait('@gqlGetSmartLinkConfigQuery');
+    cy.wait('@dpapiActivateSmartLinkAction').then((interception) => {
+      expect(interception.response?.statusCode).to.eq(200);
+
+      const body = interception.response?.body;
+      expect(body).to.have.property('successMessage');
+      expect(body.successMessage).to.be.a('string');
+    });
+  });
+
+  it('Execute SmartLink - disabled preAction ', () => {
+    cy.mountSmartLink({
+      isPreActionDisabled: true,
+    });
+    cy.wait('@gqlGetSmartLinkConfigQuery');
+    cy.get('.notifi-smartlink-action-btn')
+      .should('exist')
+      .should('be.disabled');
+  });
+
+  it('Execute SmartLink - enabled preAction ', () => {
+    cy.mountSmartLink({
+      isPreActionDisabled: false,
+    });
+    cy.wait('@gqlGetSmartLinkConfigQuery');
+    cy.get('.notifi-smartlink-action-btn')
+      .should('exist')
+      .should('be.enabled')
+      .click();
+
+    cy.get('.notifi-smartlink-action-input-textbox-input')
+      .should('exist')
+      .type('1');
+    cy.get('.notifi-smartlink-action-btn')
+      .should('exist')
+      .should('be.enabled')
+      .click();
+    cy.wait('@gqlGetSmartLinkConfigQuery');
+    cy.wait('@dpapiActivateSmartLinkAction').then((interception) => {
+      expect(interception.response?.statusCode).to.eq(200);
+
+      const body = interception.response?.body;
+      expect(body).to.have.property('successMessage');
+      expect(body.successMessage).to.be.a('string');
+    });
   });
 });
