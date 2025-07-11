41d93729d45736a0270ddfcf9a554c5ebb7cb76f
eric-notifi <eric.lee@notifi.network>
2025-07-11 15:49:41 +0900

    MVP-6454: Solana Strategy Auth test case

--- packages/notifi-frontend-client/__test__/AuthManager.test.ts
+++ packages/notifi-frontend-client/__test__/AuthManager.test.ts
@@ -3,7 +3,9 @@ import {
   AuthManager,
   type EvmUserParams,
   type NotifiFrontendConfiguration,
+  SolanaUserParams,
 } from '@notifi-network/notifi-frontend-client';
+import { generateKeyPairSigner, signBytes } from '@solana/kit';
 import expect from 'expect';
 
 import {
@@ -35,6 +37,35 @@ describe('AuthManager Unit Test', () => {
         return arrayify(signature);
       },
       walletBlockchain: blockchainType,
+    });
+    expect(userState.authorization).toHaveProperty('token');
+  });
+
+  it('login - SOLANA', async () => {
+    /* Reference: https://solana.com/developers/cookbook/wallets/sign-message */
+    const signer = await generateKeyPairSigner();
+
+    const solUserParams: SolanaUserParams = {
+      walletBlockchain: 'SOLANA',
+      walletPublicKey: signer.address,
+    };
+
+    const config: NotifiFrontendConfiguration = {
+      tenantId: dappAddress,
+      walletBlockchain: solUserParams.walletBlockchain,
+      walletPublicKey: solUserParams.walletPublicKey,
+      storageOption: { driverType: 'InMemory' },
+    };
+
+    const service = newNotifiService(config);
+    const storage = newNotifiStorage(config);
+    const authManager = new AuthManager(service, storage, config);
+    const userState = await authManager.logIn({
+      signMessage: async (message: Uint8Array) => {
+        const signature = await signBytes(signer.keyPair.privateKey, message);
+        return signature;
+      },
+      walletBlockchain: solUserParams.walletBlockchain,
     });
     expect(userState.authorization).toHaveProperty('token');
   });
