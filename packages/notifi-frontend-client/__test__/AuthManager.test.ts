55b6d24c90d962e5ae4e20508015904754edbf0f
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2025-07-11 17:27:22 +0900

    MVP-6454: Solana Strategy Auth Unit Test (#881)
    * MVP-6454: Solana Strategy Auth test case
    
    * MVP-6454-patch: try fix github action being not able to generate sol account issue
    
    * MVP-6454-patch: clean up github action solution - rather than using @solana/kit, impl more low level sign with @Solana/web3.js + tweetnacl
    
    * MVP-6454-patch: optimize test script by utilizing more low level crypto pkg 'bs58' + 'tweetnacl' to replace @solana/web3.js

--- packages/notifi-frontend-client/__test__/AuthManager.test.ts
+++ packages/notifi-frontend-client/__test__/AuthManager.test.ts
@@ -3,8 +3,11 @@ import {
   AuthManager,
   type EvmUserParams,
   type NotifiFrontendConfiguration,
+  SolanaUserParams,
 } from '@notifi-network/notifi-frontend-client';
+import bs58 from 'bs58';
 import expect from 'expect';
+import nacl from 'tweetnacl';
 
 import {
   newNotifiService,
@@ -35,6 +38,34 @@ describe('AuthManager Unit Test', () => {
         return arrayify(signature);
       },
       walletBlockchain: blockchainType,
+    });
+    expect(userState.authorization).toHaveProperty('token');
+  });
+
+  it('login - SOLANA', async () => {
+    /* Reference: https://solana.com/developers/cookbook/wallets/sign-message */
+    const signer = nacl.sign.keyPair();
+    const solUserParams: SolanaUserParams = {
+      walletBlockchain: 'SOLANA',
+      walletPublicKey: bs58.encode(signer.publicKey),
+    };
+
+    const config: NotifiFrontendConfiguration = {
+      tenantId: dappAddress,
+      walletBlockchain: solUserParams.walletBlockchain,
+      walletPublicKey: solUserParams.walletPublicKey,
+      storageOption: { driverType: 'InMemory' },
+    };
+
+    const service = newNotifiService(config);
+    const storage = newNotifiStorage(config);
+    const authManager = new AuthManager(service, storage, config);
+    const userState = await authManager.logIn({
+      signMessage: async (message: Uint8Array) => {
+        const signature = nacl.sign.detached(message, signer.secretKey);
+        return signature;
+      },
+      walletBlockchain: solUserParams.walletBlockchain,
     });
     expect(userState.authorization).toHaveProperty('token');
   });
