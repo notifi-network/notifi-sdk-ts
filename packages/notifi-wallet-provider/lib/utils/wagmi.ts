38c6e626f7a03003958a97cf257aafcebc7203b4
eric-notifi <eric.lee@notifi.network>
2025-08-11 12:03:28 +0900

    MVP-6528: Fix WalletConnect sign failure due to un configured chains

--- packages/notifi-wallet-provider/lib/utils/wagmi.ts
+++ packages/notifi-wallet-provider/lib/utils/wagmi.ts
@@ -1,23 +1,51 @@
 import { createConfig, http } from 'wagmi';
-import { mainnet, polygon } from 'wagmi/chains';
+import * as chains from 'wagmi/chains';
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
 
-// TODO: use EVM_BLOCKCHAINS
+/* Currently supported EVM chains by Notifi */
+// TODO: Allow users to pass in their own chains
+const notifiEvmChains = [
+  chains.mainnet,
+  chains.polygon,
+  chains.avalanche,
+  chains.arbitrum,
+  chains.bsc,
+  // chains.botanix, // Wagmi does not support yet
+  chains.bob,
+  // chains.monad, // Wagmi does not support yet
+  chains.base,
+  chains.blast,
+  chains.celo,
+  chains.mantle,
+  chains.scroll,
+  chains.linea,
+  chains.manta,
+  chains.berachain,
+  chains.unichain,
+  chains.optimism,
+  chains.root,
+  chains.sei,
+  chains.sonic,
+  chains.ink,
+  chains.swellchain,
+  // chains.rome, // Wagmi does not support yet
+  chains.zksync,
+] as Parameters<typeof createConfig>[0]['chains'];
+
 export const config = createConfig({
-  chains: [mainnet, polygon],
+  chains: notifiEvmChains,
   connectors: [
     injected(),
-    coinbaseWallet({ appName: 'Notifi' }),
+    coinbaseWallet({ appName: 'Notifi' }), // TODO: Allow users to pass in their own appName
     walletConnect({
-      projectId: '632a105feb9cf8380428a4f240eb6f13',
+      projectId: '632a105feb9cf8380428a4f240eb6f13', // TODO: Allow users to pass in their own projectId
       qrModalOptions: {
         explorerExcludedWalletIds: 'ALL',
         enableExplorer: false,
       },
     }),
   ],
-  transports: {
-    [mainnet.id]: http(),
-    [polygon.id]: http(),
-  },
+  transports: Object.fromEntries(
+    notifiEvmChains.map((chain) => [chain.id, http()]),
+  ),
 });
