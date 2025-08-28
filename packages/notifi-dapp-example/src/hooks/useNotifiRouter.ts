02e8c009f387117481901e35a34fb1bd1f4ef255
eric-notifi <eric.lee@notifi.network>
2025-08-28 17:22:36 +0900

    MVP-6548: Fix side effect of enabling switching account

--- packages/notifi-dapp-example/src/hooks/useNotifiRouter.ts
+++ packages/notifi-dapp-example/src/hooks/useNotifiRouter.ts
@@ -5,6 +5,7 @@ import {
   useNotifiUserSettingContext,
 } from '@notifi-network/notifi-react';
 import { useWallets } from '@notifi-network/notifi-wallet-provider';
+import { usePathname } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 
 import { useRouterAsync } from './useRouterAsync';
@@ -21,8 +22,10 @@ export const useNotifiRouter = () => {
   const { ftuStage, isLoading: isLoadingFtu } = useNotifiUserSettingContext();
   const { wallets, selectedWallet } = useWallets();
   const isLoginStarted = useRef(false);
+  const pathname = usePathname();
 
   useEffect(() => {
+    console.log(2, 'rerender routing effect');
     if (!frontendClientStatus.isInitialized) return;
     if (frontendClientStatus.isExpired) {
       handleRoute('/notifi/expiry');
@@ -30,9 +33,11 @@ export const useNotifiRouter = () => {
     }
     if (frontendClientStatus.isAuthenticated && !isLoadingFtu) {
       if (ftuStage === FtuStage.Done) {
+        console.log(4, 'rerender routing effect done');
         handleRoute('/notifi/dashboard');
         return;
       } else {
+        console.log(5, 'rerender routing effect ftu');
         handleRoute('/notifi/ftu');
         return;
       }
@@ -40,7 +45,13 @@ export const useNotifiRouter = () => {
   }, [frontendClientStatus, ftuStage, isLoadingFtu]);
 
   useEffect(() => {
+    console.log(3, 'rerender login effect');
     if (isLoginStarted.current || frontendClientStatus.isAuthenticated) return;
+    if (pathname !== '/') {
+      console.log(6, 'rerender login effect redirect');
+      handleRoute('/');
+      return;
+    }
     isLoginStarted.current = true;
     login(); // NOTE: No need handle error & loading, use isLoading & error hook instead
   }, []);
