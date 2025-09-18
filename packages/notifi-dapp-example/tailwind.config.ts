0bcc97bd04aa0ff917f9b20ee54323095285eaa4
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2025-09-19 08:52:45 +0900

    MVP-6570: Update next.config.mjs and related parts for dapp-example  (#915)
    * MVP-6570: Structural update - update next.config.mjs and related parts
    
    * MVP-6570: Update browser-list db for next.js
    
    * MVP-6570: fix dapp-example tailwind config conflict - content & purge

--- packages/notifi-dapp-example/tailwind.config.ts
+++ packages/notifi-dapp-example/tailwind.config.ts
@@ -1,17 +1,8 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  purge: {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    options: {
-      safelist: ['html', 'body'],
-    },
-  },
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  safelist: ['html', 'body'],
   theme: {
     extend: {
       width: {
