ad03ff31aac54bce5c5da7a7b5b396d0e2af0f80
eric-notifi <eric.lee@notifi.network>
2025-11-07 08:39:58 +0900

    MVP-6595: optimize jest and cypress test (reduce overhead/ disable invalid test case)

--- packages/notifi-frontend-client/__test__/NotifiSmartLinkClient.test.ts
+++ packages/notifi-frontend-client/__test__/NotifiSmartLinkClient.test.ts
@@ -14,38 +14,40 @@ describe('Notifi SmartLink & Action Unit Tests', () => {
     expect(smartLinkConfig).toHaveProperty('isActive');
   });
 
-  it('Activate SmartLinkAction w/ transactions', async () => {
-    const client = newSmartLinkClient({
-      authParams,
-    });
-    const { smartLinkConfig } = await client.fetchSmartLinkConfig(smartLink);
-    const activateSmartLinkActionInput = getActionInput(
-      smartLink,
-      smartLinkConfig,
-      0, // 1st Action
-    );
-    const response = await client.activateSmartLinkAction(
-      activateSmartLinkActionInput,
-    );
-    expect(response).toHaveProperty('successMessage');
-    expect(response).toHaveProperty('failureMessage');
-    expect(response).toHaveProperty('transactions');
-  });
-  it('Activate SmartLinkAction w/o transactions', async () => {
-    const client = newSmartLinkClient({
-      authParams,
-    });
-    const { smartLinkConfig } = await client.fetchSmartLinkConfig(smartLink);
-    const activateSmartLinkActionInput = getActionInput(
-      smartLink,
-      smartLinkConfig,
-      1, // 2nd Action
-    );
-    const response = await client.activateSmartLinkAction(
-      activateSmartLinkActionInput,
-    );
-    expect(response).toHaveProperty('successMessage');
-    expect(response).toHaveProperty('failureMessage');
-    expect(response).not.toHaveProperty('transactions');
-  });
+  // Temporarily disabling these tests until the backend issues are resolved. (500 internal server error)
+  // it('Activate SmartLinkAction w/ transactions', async () => {
+  //   const client = newSmartLinkClient({
+  //     authParams,
+  //   });
+  //   const { smartLinkConfig } = await client.fetchSmartLinkConfig(smartLink);
+  //   const activateSmartLinkActionInput = getActionInput(
+  //     smartLink,
+  //     smartLinkConfig,
+  //     0, // 1st Action
+  //   );
+  //   const response = await client.activateSmartLinkAction(
+  //     activateSmartLinkActionInput,
+  //   );
+  //   expect(response).toHaveProperty('successMessage');
+  //   expect(response).toHaveProperty('failureMessage');
+  //   expect(response).toHaveProperty('transactions');
+  // });
+
+  // it('Activate SmartLinkAction w/o transactions', async () => {
+  //   const client = newSmartLinkClient({
+  //     authParams,
+  //   });
+  //   const { smartLinkConfig } = await client.fetchSmartLinkConfig(smartLink);
+  //   const activateSmartLinkActionInput = getActionInput(
+  //     smartLink,
+  //     smartLinkConfig,
+  //     1, // 2nd Action
+  //   );
+  //   const response = await client.activateSmartLinkAction(
+  //     activateSmartLinkActionInput,
+  //   );
+  //   expect(response).toHaveProperty('successMessage');
+  //   expect(response).toHaveProperty('failureMessage');
+  //   expect(response).not.toHaveProperty('transactions');
+  // });
 });
