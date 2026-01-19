c4011c9d71a45c1bda437f2f2c944b1a50fa739b
eric-notifi <eric.lee@notifi.network>
2026-01-19 12:42:01 +0900

    MVP-6758: Add back missing payload errors to SDK mutation schema

--- packages/notifi-graphql/lib/gql/mutations/verifyXmtpTargetViaXip42.gql.ts
+++ packages/notifi-graphql/lib/gql/mutations/verifyXmtpTargetViaXip42.gql.ts
@@ -1,12 +1,20 @@
 import { gql } from 'graphql-request';
 
+import { ErrorFragments } from '../fragments/ErrorFragments.gql';
 import { VerifyXmtpTargetViaXip42ResponseFragment } from '../fragments/Web3TargetFragment.gql';
 
 export const VerifyXmtpTargetViaXip42 = gql`
   mutation verifyXmtpTargetViaXip42($input: VerifyXmtpTargetViaXip42Input!) {
     verifyXmtpTargetViaXip42(input: $input) {
       ...VerifyXmtpTargetViaXip42ResponseFragment
+      errors {
+        ...Web3TargetNotFoundErrorFragment
+        ...UnauthorizedXmtpAccountErrorFragment
+        ...UnsupportedWeb3ProtocolErrorFragment
+        ...XmtpAccountIsntConnectedWalletErrorFragment
+      }
     }
   }
   ${VerifyXmtpTargetViaXip42ResponseFragment}
+  ${ErrorFragments}
 `;
