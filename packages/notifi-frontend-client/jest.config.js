19cb5eac87031190d2373c16cf33201f02fc7f9e
Eric Lee <happyeric77@gmail.com>
2026-02-23 21:11:42 +0900

    MVP-6780: fix: upgrade @cosmjs/amino from 0.33.1 to 0.38.1 in frontend-client tests

--- packages/notifi-frontend-client/jest.config.js
+++ packages/notifi-frontend-client/jest.config.js
@@ -8,4 +8,17 @@ module.exports = {
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testTimeout: 10000, // 10 seconds
+  transformIgnorePatterns: ['node_modules/(?!(@cosmjs|@scure|@noble)/)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        allowJs: true,
+        esModuleInterop: true,
+      },
+    },
+  },
+  moduleNameMapper: {
+    '^@scure/(.*)$': '<rootDir>/node_modules/@scure/$1',
+    '^@noble/(.*)$': '<rootDir>/node_modules/@noble/$1',
+  },
 };
