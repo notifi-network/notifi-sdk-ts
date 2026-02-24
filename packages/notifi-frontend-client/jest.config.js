874c636903210e4e2ef7a5d7872b391421e59191
eric-notifi <eric.lee@notifi.network>
2026-02-24 15:18:41 +0900

    MVP-6780: optimize jest config / remove unused dep - crypto-browserify

--- packages/notifi-frontend-client/jest.config.js
+++ packages/notifi-frontend-client/jest.config.js
@@ -8,17 +8,17 @@ module.exports = {
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testTimeout: 10000, // 10 seconds
+  // Transform ESM modules from @cosmjs, @scure, @noble
   transformIgnorePatterns: ['node_modules/(?!(@cosmjs|@scure|@noble)/)'],
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        allowJs: true,
-        esModuleInterop: true,
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          allowJs: true,
+          esModuleInterop: true,
+        },
       },
-    },
-  },
-  moduleNameMapper: {
-    '^@scure/(.*)$': '<rootDir>/node_modules/@scure/$1',
-    '^@noble/(.*)$': '<rootDir>/node_modules/@noble/$1',
+    ],
   },
 };
